a4785a717f22d80554e63a98e07900b69029d31c
Thomas Krause <thomaskrause@posteo.de>
2019-08-29 18:24:20 +0200

    Show example SCorpusGraph

--- bundles/org.corpus_tools.hexatomic.corpusedit/src/main/java/org/corpus_tools/hexatomic/corpusedit/parts/CorpusTreeProvider.java
+++ bundles/org.corpus_tools.hexatomic.corpusedit/src/main/java/org/corpus_tools/hexatomic/corpusedit/parts/CorpusTreeProvider.java
@@ -1,20 +1,41 @@
 package org.corpus_tools.hexatomic.corpusedit.parts;
 
-import org.eclipse.jface.viewers.ArrayContentProvider;
+import java.util.List;
+
+import org.corpus_tools.salt.SALT_TYPE;
+import org.corpus_tools.salt.common.SCorpusGraph;
+import org.corpus_tools.salt.core.SNode;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 
 public class CorpusTreeProvider implements ITreeContentProvider {
-	
-	
 
 	@Override
 	public Object[] getElements(Object inputElement) {
-		return ArrayContentProvider.getInstance().getElements(inputElement);
+		if (inputElement instanceof SCorpusGraph) {
+			SCorpusGraph g = (SCorpusGraph) inputElement;
+			return g.getRoots().toArray();
+		} else if (inputElement instanceof SNode) {
+			SNode n = (SNode) inputElement;
+
+			return new String[] { n.getName() };
+
+		} else {
+			return null;
+		}
 	}
 
 	@Override
 	public Object[] getChildren(Object parentElement) {
-		return null;
+
+		if (parentElement instanceof SNode) {
+			SNode n = (SNode) parentElement;
+
+			List<SNode> children = n.getGraph().getChildren(n, null);
+			return children.toArray();
+
+		} else {
+			return null;
+		}
 	}
 
 	@Override
@@ -24,7 +45,16 @@ public class CorpusTreeProvider implements ITreeContentProvider {
 
 	@Override
 	public boolean hasChildren(Object element) {
-		return false;
+		if(element instanceof SCorpusGraph) {
+			return !((SCorpusGraph) element).getRoots().isEmpty();
+		} else if (element instanceof SNode) {
+			SNode n = (SNode) element;
+
+			List<SNode> children = n.getGraph().getChildren(n, null);
+			return !children.isEmpty();
+		} else {
+			return false;
+		}
 	}
 
 }
