99e4426553b5c2a19e12ca16d8162ee1bcb227bd
Thomas Krause <thomaskrause@posteo.de>
2019-08-30 11:19:32 +0200

    Add a simple Project Manager to core that can be injected

---
+++ bundles/org.corpus_tools.hexatomic.core/src/main/java/org/corpus_tools/hexatomic/core/ProjectManager.java
@@ -0,0 +1,28 @@
+package org.corpus_tools.hexatomic.core;
+
+import org.corpus_tools.salt.SaltFactory;
+import org.corpus_tools.salt.common.SaltProject;
+import org.eclipse.e4.core.di.annotations.Creatable;
+
+/**
+ * Manages creating, opening and saving projects.
+ * 
+ * @author Thomas Krause {@literal <krauseto@hu-berlin.de>}
+ *
+ */
+@Creatable
+public class ProjectManager {
+
+	private SaltProject project;
+	
+	public ProjectManager() {
+		// create an empty project
+		this.project = SaltFactory.createSaltProject();
+	}
+
+	public SaltProject getProject() {
+		return project;
+	}
+	
+	
+}
