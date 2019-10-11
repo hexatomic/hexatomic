d6e2542eb5c37fbb5f8c30cf59bd10d72890de93
Thomas Krause <thomaskrause@posteo.de>
2019-10-11 15:39:02 +0200

    Get the document ID from the peristed state of the Part

--- bundles/org.corpus_tools.hexatomic.textviewer/src/main/java/org/corpus_tools/hexatomic/textviewer/TextViewer.java
+++ bundles/org.corpus_tools.hexatomic.textviewer/src/main/java/org/corpus_tools/hexatomic/textviewer/TextViewer.java
@@ -1,29 +1,29 @@
- 
-package org.corpus_tools.hexatomic.textviewer;
 
+package org.corpus_tools.hexatomic.textviewer;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.SWT;
-
 
 public class TextViewer {
-	
+
 	@Inject
 	public TextViewer() {
-		
+
 	}
-	
+
 	@PostConstruct
-	public void postConstruct(Composite parent) {
+	public void postConstruct(Composite parent, MPart part) {
 		
+		String documentID = part.getPersistedState().get("org.corpus_tools.hexatomic.document-id");
+
 		Label lblText = new Label(parent, SWT.NONE);
-		lblText.setText("Hello World");
-		
-		
+		lblText.setText("Hello " + documentID);
+
 	}
-	
+
 }
