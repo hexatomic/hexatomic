8fa11d8a1d5223030ae7acc36ecb7884c1bca63b
Thomas Krause <thomaskrause@posteo.de>
2019-10-22 14:54:13 +0200

    Externalize string

--- bundles/org.corpus_tools.hexatomic.core/src/main/java/org/corpus_tools/hexatomic/core/handlers/OpenSaltDocumentHandler.java
+++ bundles/org.corpus_tools.hexatomic.core/src/main/java/org/corpus_tools/hexatomic/core/handlers/OpenSaltDocumentHandler.java
@@ -15,34 +15,37 @@ import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
 
 /**
  * Handler to open a document with a specific editor.
+ * 
  * @author Thomas Krause
  *
  */
 public class OpenSaltDocumentHandler {
 
+	public static final String COMMAND_PARAM_EDITOR_ID = "org.corpus_tools.hexatomic.core.commandparameter.editor-id";
 	public static final String DOCUMENT_ID = "org.corpus_tools.hexatomic.document-id";
 
 	/**
 	 * Opens the currently selected document with the given editor.
+	 * 
 	 * @param projectManager
 	 * @param modelService
 	 * @param partService
 	 * @param selectionService
-	 * @param editorID The model ID of the editor PartDescription to use a template
+	 * @param editorID         The model ID of the editor PartDescription to use a
+	 *                         template
 	 */
 	@Execute
-	public static void execute(ProjectManager projectManager, EModelService modelService,
-			EPartService partService, ESelectionService selectionService,
-			@Named("org.corpus_tools.hexatomic.core.commandparameter.editor-id") String editorID) {
-		
+	public static void execute(ProjectManager projectManager, EModelService modelService, EPartService partService,
+			ESelectionService selectionService, @Named(COMMAND_PARAM_EDITOR_ID) String editorID) {
+
 		// get currently selected document
 		Object selection = selectionService.getSelection();
-		if(selection instanceof SDocument) {
-			
+		if (selection instanceof SDocument) {
+
 			SDocument document = (SDocument) selection;
-	
-			if(document.getDocumentGraph() == null) {
-				if(document.getDocumentGraphLocation() == null) {
+
+			if (document.getDocumentGraph() == null) {
+				if (document.getDocumentGraphLocation() == null) {
 					// create a new document graph, because no one exists yet
 					document.createDocumentGraph();
 				} else {
@@ -55,14 +58,16 @@ public class OpenSaltDocumentHandler {
 			MPart editorPart = partService.createPart(editorID);
 			editorPart.setLabel(document.getName());
 			editorPart.getPersistedState().put(OpenSaltDocumentHandler.DOCUMENT_ID, document.getId());
-			
+
 			partService.showPart(editorPart, PartState.ACTIVATE);
 
 		}
 	}
-	
+
 	/**
-	 * Checks whether the currently selected object is a document that can be opened.
+	 * Checks whether the currently selected object is a document that can be
+	 * opened.
+	 * 
 	 * @param selectionService
 	 * @return
 	 */
