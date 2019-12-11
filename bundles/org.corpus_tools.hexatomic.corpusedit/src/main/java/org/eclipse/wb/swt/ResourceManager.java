13b992b2d394b738cc3994013857e236ab13f199
Stephan Druskat <sdruskat@users.noreply.github.com>
2019-12-11 18:35:13 +0100

    Update bundles/org.corpus_tools.hexatomic.corpusedit/src/main/java/org/eclipse/wb/swt/ResourceManager.java

--- bundles/org.corpus_tools.hexatomic.corpusedit/src/main/java/org/eclipse/wb/swt/ResourceManager.java
+++ bundles/org.corpus_tools.hexatomic.corpusedit/src/main/java/org/eclipse/wb/swt/ResourceManager.java
@@ -2,7 +2,10 @@
  * Copyright (c) 2011 Google, Inc. and others 
  * All rights reserved. This program and the accompanying
  * materials are made available under the terms of the Eclipse Public License v1.0 which accompanies
- * this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html Contributors:
+ * this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html 
+ *
+ * <p>
+ * Contributors:
  * Google, Inc. - initial API and implementation Wim Jongman - 1.8 and higher compliance
  *******************************************************************************/
 
