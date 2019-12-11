a176d5c1138d0f216b0bb4cdc94c40a7f71d3869
Stephan Druskat <mail@sdruskat.net>
2019-12-11 19:10:39 +0100

    Fix formatting

--- bundles/org.corpus_tools.hexatomic.corpusedit/src/main/java/org/eclipse/wb/swt/ResourceManager.java
+++ bundles/org.corpus_tools.hexatomic.corpusedit/src/main/java/org/eclipse/wb/swt/ResourceManager.java
@@ -4,8 +4,7 @@
  * materials are made available under the terms of the Eclipse Public License v1.0 which accompanies
  * this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html 
  *
- * <p>
- * Contributors:
+ * <p>Contributors:
  * Google, Inc. - initial API and implementation 
  * Wim Jongman - 1.8 and higher compliance
  * </p>
