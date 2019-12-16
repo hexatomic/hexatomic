6f8b5e5367cee8859efc4f8dbc6b06640020494d
Thomas Krause <thomaskrause@posteo.de>
2019-12-16 14:13:06 +0100

    Update view when annotation graph was changed

--- bundles/org.corpus_tools.hexatomic.edit.graph/src/main/java/org/corpus_tools/hexatomic/graph/GraphEditor.java
+++ bundles/org.corpus_tools.hexatomic.edit.graph/src/main/java/org/corpus_tools/hexatomic/graph/GraphEditor.java
@@ -51,6 +51,8 @@ import org.corpus_tools.salt.common.SToken;
 import org.corpus_tools.salt.core.SAnnotation;
 import org.corpus_tools.salt.core.SNode;
 import org.corpus_tools.salt.core.SRelation;
+import org.corpus_tools.salt.extensions.notification.Listener;
+import org.corpus_tools.salt.graph.GRAPH_ATTRIBUTES;
 import org.corpus_tools.salt.graph.IdentifiableElement;
 import org.corpus_tools.salt.util.DataSourceSequence;
 import org.eclipse.e4.ui.di.UISynchronize;
@@ -136,6 +138,15 @@ public class GraphEditor {
   @PostConstruct
   public void postConstruct(Composite parent, MPart part) {
 
+    projectManager.addListener(new Listener() {
+
+      @Override
+      public void notify(NOTIFICATION_TYPE type, GRAPH_ATTRIBUTES attribute, Object oldValue,
+          Object newValue, Object container) {
+        sync.asyncExec(() -> updateView(false));
+      }
+
+    });
 
     parent.setLayout(new FillLayout(SWT.VERTICAL));
 
