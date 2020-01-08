4745cc50484dae30ebb54b65130dbb4d4d0cf407
Stephan Druskat <mail@sdruskat.net>
2020-01-08 19:32:53 +0100

    Make opened document available in editor

--- bundles/org.corpus_tools.hexatomic.edit.grid/src/main/java/org/corpus_tools/hexatomic/edit/grid/GridEditor.java
+++ bundles/org.corpus_tools.hexatomic.edit.grid/src/main/java/org/corpus_tools/hexatomic/edit/grid/GridEditor.java
@@ -1,8 +1,13 @@
 
 package org.corpus_tools.hexatomic.edit.grid;
 
+import java.util.Optional;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import org.corpus_tools.hexatomic.core.ProjectManager;
+import org.corpus_tools.salt.common.SDocument;
+import org.corpus_tools.salt.common.SDocumentGraph;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.swt.widgets.Composite;
 
 public class GridEditor {
@@ -14,9 +19,24 @@ public class GridEditor {
 
   }
 
+  @Inject
+  private ProjectManager projectManager;
+
+  @Inject
+  private MPart thisPart;
+
   @PostConstruct
   public void postConstruct(Composite parent) {
-    log.debug("Starting Grid Editor.");
+    log.debug("Starting Grid Editor for document '{}'.", getGraph().getDocument().getName());
+  }
+
+  private SDocumentGraph getGraph() {
+    String documentID = thisPart.getPersistedState().get("org.corpus_tools.hexatomic.document-id");
+    Optional<SDocument> doc = projectManager.getDocument(documentID);
+    if (doc.isPresent()) {
+      return doc.get().getDocumentGraph();
+    }
+    return null;
   }
 
 }
