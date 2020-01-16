c76da36b31d301d7eacda1aa15fc62c921baad95
Stephan Druskat <mail@sdruskat.net>
2020-01-16 16:56:08 +0100

    Add dropdown box for STextualDS selection
    - Available textual ds can be selected
    - Selection is passed to the ESelectionService which other parts can subscribe to and consume the selection
    - Own dog food is eaten as GridEditor also consumes selection of textual ds via ESelectionService

--- bundles/org.corpus_tools.hexatomic.edit.grid/src/main/java/org/corpus_tools/hexatomic/edit/grid/GridEditor.java
+++ bundles/org.corpus_tools.hexatomic.edit.grid/src/main/java/org/corpus_tools/hexatomic/edit/grid/GridEditor.java
@@ -21,21 +21,30 @@
 
 package org.corpus_tools.hexatomic.edit.grid;
 
-import java.util.Optional;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import javax.inject.Named;
 import org.corpus_tools.hexatomic.core.ProjectManager;
 import org.corpus_tools.hexatomic.edit.grid.data.GraphDataProvider;
 import org.corpus_tools.salt.common.SDocument;
 import org.corpus_tools.salt.common.SDocumentGraph;
+import org.corpus_tools.salt.common.STextualDS;
+import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
 import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ComboViewer;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.nebula.widgets.nattable.NatTable;
 import org.eclipse.nebula.widgets.nattable.data.IDataProvider;
 import org.eclipse.nebula.widgets.nattable.layer.DataLayer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 
 /**
@@ -48,16 +57,30 @@ public class GridEditor {
 
   private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(GridEditor.class);
 
+  /**
+   * Consumes the selection of an {@link STextualDS} from the {@link ESelectionService}.
+   * 
+   * @param ds The {@link STextualDS} that has been selected via the {@link ESelectionService}.
+   */
   @Inject
-  public GridEditor() {
-
+  void setSelection(@Optional @Named(IServiceConstants.ACTIVE_SELECTION) STextualDS ds) {
+    if (ds != null) {
+      log.debug("The textual data source {} has been selected.", ds.getId());
+    } else {
+      // Do nothing
+    }
   }
+
+  @Inject
+  private ESelectionService selectionService;
 
   @Inject
   private ProjectManager projectManager;
 
   @Inject
   private MPart thisPart;
+
+  private SDocumentGraph graph;
 
   /**
    * Creates the grid that contains the data of the {@link SDocumentGraph}.
@@ -66,7 +89,8 @@ public class GridEditor {
    */
   @PostConstruct
   public void postConstruct(Composite parent) {
-    log.debug("Starting Grid Editor for document '{}'.", getGraph().getDocument().getName());
+    this.graph = getGraph();
+    log.debug("Starting Grid Editor for document '{}'.", graph.getDocument().getName());
 
     parent.setLayout(new GridLayout());
 
@@ -74,13 +98,10 @@ public class GridEditor {
      * FIXME Add dropdown for text selection. Redo grid once text is selected. TODO REMOVE: One grid
      * per text, i.e., grids only ever work on one text!
      */
-    Combo dropdown = new Combo(parent, SWT.NONE);
-    dropdown.add("Select data source ...");
-    dropdown.select(0);
-
+    addTextSelectionDropdown(parent);
 
     // Create data provider & layer, data layer needs to be most bottom layer in the stack!
-    IDataProvider bodyDataProvider = new GraphDataProvider(getGraph());
+    IDataProvider bodyDataProvider = new GraphDataProvider(graph);
     final DataLayer bodyDataLayer = new DataLayer(bodyDataProvider);
 
     // Create and configure NatTable
@@ -90,9 +111,57 @@ public class GridEditor {
     GridDataFactory.fillDefaults().grab(true, true).applyTo(natTable);
   }
 
+  /**
+   * Creates a dropdown presenting the available {@link STextualDS}s from the
+   * {@link SDocumentGraph}. When a text is selected, the selection is forwarded to the injectable
+   * {@link ESelectionService}, so that other parts can consume the selection. This part also
+   * consumes the selection, in {@link #setSelection(STextualDS)}.
+   * 
+   * THe following example shows how other parts can consume the selection.
+   * 
+   * <pre>
+   * {@code
+   *   &#64;Inject void setSelection(@Optional @Named(IServiceConstants.ACTIVE_SELECTION) 
+   *       Object o) { 
+   *     if (o instanceof STextualDS) { 
+   *       STextualDS ds = (STextualDS) o;
+   *       log.debug("The text {} has been selected.", ds.getId()); 
+   *     } 
+   *   }
+   * }
+   * </pre>
+   * 
+   * @param parent The parent composite
+   */
+  private void addTextSelectionDropdown(Composite parent) {
+    final ComboViewer viewer = new ComboViewer(parent, SWT.READ_ONLY);
+    viewer.setContentProvider(ArrayContentProvider.getInstance());
+    viewer.setLabelProvider(new LabelProvider() {
+      @Override
+      public String getText(Object element) {
+        if (element instanceof STextualDS) {
+          STextualDS ds = (STextualDS) element;
+          return ds.getName() + " (" + ds.getId() + ")";
+        }
+        return super.getText(element);
+      }
+    });
+    viewer.addSelectionChangedListener(new ISelectionChangedListener() {
+      @Override
+      public void selectionChanged(SelectionChangedEvent event) {
+        IStructuredSelection selection = (IStructuredSelection) event.getSelection();
+        if (selection.size() > 0 && selection.getFirstElement() instanceof STextualDS) {
+          selectionService.setSelection(
+              selection.size() == 1 ? selection.getFirstElement() : selection.toArray());
+        }
+      }
+    });
+    viewer.setInput(graph.getTextualDSs());
+  }
+
   private SDocumentGraph getGraph() {
     String documentID = thisPart.getPersistedState().get("org.corpus_tools.hexatomic.document-id");
-    Optional<SDocument> doc = projectManager.getDocument(documentID);
+    java.util.Optional<SDocument> doc = projectManager.getDocument(documentID);
     if (doc.isPresent()) {
       return doc.get().getDocumentGraph();
     }
