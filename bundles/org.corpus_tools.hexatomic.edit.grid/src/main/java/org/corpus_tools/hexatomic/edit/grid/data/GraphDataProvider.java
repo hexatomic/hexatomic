307bfbd41376d11a333b144df591ac12dbd1b4fd
Stephan Druskat <mail@sdruskat.net>
2020-01-16 11:22:07 +0100

    Display token annotation values in grid

--- bundles/org.corpus_tools.hexatomic.edit.grid/src/main/java/org/corpus_tools/hexatomic/edit/grid/data/GraphDataProvider.java
+++ bundles/org.corpus_tools.hexatomic.edit.grid/src/main/java/org/corpus_tools/hexatomic/edit/grid/data/GraphDataProvider.java
@@ -21,10 +21,14 @@
 
 package org.corpus_tools.hexatomic.edit.grid.data;
 
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 import org.corpus_tools.salt.common.SDocumentGraph;
 import org.corpus_tools.salt.common.SToken;
+import org.corpus_tools.salt.core.SAnnotation;
 import org.eclipse.nebula.widgets.nattable.NatTable;
 import org.eclipse.nebula.widgets.nattable.data.IDataProvider;
 
@@ -42,6 +46,8 @@ public class GraphDataProvider implements IDataProvider {
 
   private final SDocumentGraph graph;
   private int columnCount = 1;
+  private List<String> columnHeaderLabels =
+      new ArrayList<String>(Arrays.asList(new String[] {"Token"}));
 
   /**
    * Initializes the graph to grid resolution.
@@ -55,11 +61,20 @@ public class GraphDataProvider implements IDataProvider {
 
   private void resolveGraph() {
     log.debug("Starting to resolve SDocumentGraph of {}.", graph.getDocument());
-    // Count token annotations and add to column count
-    Set<String> tokenAnnotations = new HashSet<>();
-    for (SToken token : graph.getTokens()) {
 
+    // Count token annotations and add to column count
+    Set<String> tokenAnnotationQNames = new TreeSet<>();
+    for (SToken token : graph.getTokens()) {
+      Set<SAnnotation> tokAnnos = token.getAnnotations();
+      for (SAnnotation anno : tokAnnos) {
+        tokenAnnotationQNames.add(anno.getQName());
+      }
     }
+    columnCount += tokenAnnotationQNames.size();
+    // Add in alphabetical order the newly added qualified annotation names.
+    columnHeaderLabels.addAll(tokenAnnotationQNames);
+    log.debug("Resolved token annotations for {}.", graph.getDocument());
+
     log.debug("Finished resolving SDocumentGraph of {}.", graph.getDocument());
   }
 
@@ -68,6 +83,18 @@ public class GraphDataProvider implements IDataProvider {
     if (columnIndex == 0) {
       // Token text
       return graph.getText(graph.getSortedTokenByText().get(rowIndex));
+    } else {
+      SAnnotation anno = null;
+
+      // Get token and see if it has such an annotation
+      SToken tok = graph.getSortedTokenByText().get(rowIndex);
+      anno = tok.getAnnotation(columnHeaderLabels.get(columnIndex));
+      if (anno != null) {
+        return anno.getValue_STEXT();
+      }
+
+      // TODO No token annotation found, so check span annotations
+
     }
     return null;
   }
