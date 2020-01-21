e9d70a53adeec5bc329f8a3da682829b32a2be39
Stephan Druskat <mail@sdruskat.net>
2020-01-21 22:31:44 +0100

    Implement simple selection of nodes for display based on data source

--- bundles/org.corpus_tools.hexatomic.edit.grid/src/main/java/org/corpus_tools/hexatomic/edit/grid/data/GraphDataProvider.java
+++ bundles/org.corpus_tools.hexatomic.edit.grid/src/main/java/org/corpus_tools/hexatomic/edit/grid/data/GraphDataProvider.java
@@ -26,11 +26,16 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
+import org.corpus_tools.salt.SALT_TYPE;
 import org.corpus_tools.salt.common.SDocumentGraph;
 import org.corpus_tools.salt.common.SSpan;
 import org.corpus_tools.salt.common.SStructuredNode;
+import org.corpus_tools.salt.common.STextualDS;
+import org.corpus_tools.salt.common.STextualRelation;
 import org.corpus_tools.salt.common.SToken;
 import org.corpus_tools.salt.core.SAnnotation;
+import org.corpus_tools.salt.core.SRelation;
+import org.corpus_tools.salt.util.DataSourceSequence;
 import org.eclipse.nebula.widgets.nattable.NatTable;
 import org.eclipse.nebula.widgets.nattable.data.IDataProvider;
 
@@ -46,10 +51,16 @@ public class GraphDataProvider implements IDataProvider {
       org.slf4j.LoggerFactory.getLogger(GraphDataProvider.class);
 
 
+  private STextualDS ds = null;
   private final SDocumentGraph graph;
   private int columnCount = 1;
+  private int tokenCount = 1;
+  private final List<SToken> dsTokens = new ArrayList<SToken>();
+  private final List<SSpan> dsSpans = new ArrayList<SSpan>();
   private List<String> columnHeaderLabels =
       new ArrayList<String>(Arrays.asList(new String[] {"Token"}));
+
+
 
   /**
    * Initializes the graph to grid resolution.
@@ -58,17 +69,38 @@ public class GraphDataProvider implements IDataProvider {
    */
   public GraphDataProvider(SDocumentGraph graph) {
     this.graph = graph;
-    resolveGraph();
   }
 
   private void resolveGraph() {
-    log.debug("Starting to resolve SDocumentGraph of {}.", graph.getDocument());
+    dsTokens.clear();
+    dsSpans.clear();
+    log.debug("Starting to resolve SDocumentGraph of {} for data source {}.", graph.getDocument(),
+        ds);
+
+    // Resolve which tokens should be taken into account based on ds and save to dsTokens.
+    for (SRelation<?, ?> inRel : ds.getInRelations()) {
+      if (inRel instanceof STextualRelation) {
+        // Source can only be token
+        dsTokens.add((SToken) inRel.getSource());
+      }
+    }
+    tokenCount = dsTokens.size();
+
+    // FIXME Resolve which spans should be taken into account based on ds.
+    for (SSpan span : graph.getSpans()) {
+      List<DataSourceSequence> overlappedSequences =
+          graph.getOverlappedDataSourceSequence(span, SALT_TYPE.STEXT_OVERLAPPING_RELATION);
+      for (DataSourceSequence seq : overlappedSequences) {
+        if (seq.getDataSource() == ds)
+          dsSpans.add(span);
+      }
+    }
 
     // Count token annotations and add to column count
-    resolveAnnotations(new ArrayList<SStructuredNode>(graph.getTokens()));
+    resolveAnnotations(new ArrayList<SStructuredNode>(dsTokens));
 
     // Count span annotations and add to column count
-    resolveAnnotations(new ArrayList<SStructuredNode>(graph.getSpans()));
+    resolveAnnotations(new ArrayList<SStructuredNode>(dsSpans));
 
     log.debug("Finished resolving SDocumentGraph of {}.", graph.getDocument());
   }
@@ -90,10 +122,16 @@ public class GraphDataProvider implements IDataProvider {
 
   @Override
   public Object getDataValue(int columnIndex, int rowIndex) {
-    if (columnIndex == 0) {
-      // Token text
-      return graph.getText(graph.getSortedTokenByText().get(rowIndex));
+    if (ds == null) {
+      if (columnIndex == 0 && rowIndex == 0) {
+        return "Please select data source!";
+      }
     } else {
+      if (columnIndex == 0) {
+        // Token text
+        // FIXME Change to select based on ds!
+        return graph.getText(graph.getSortedTokenByText().get(rowIndex));
+      }
       String annotationQName = columnHeaderLabels.get(columnIndex);
       SAnnotation anno = null;
 
@@ -126,12 +164,23 @@ public class GraphDataProvider implements IDataProvider {
 
   @Override
   public int getColumnCount() {
+    if (ds == null) {
+      return 1;
+    }
     return columnCount;
   }
 
   @Override
   public int getRowCount() {
-    return graph.getTokens().size();
+    return tokenCount;
+  }
+
+  /**
+   * @param ds the ds to set
+   */
+  public void setDs(STextualDS ds) {
+    this.ds = ds;
+    resolveGraph();
   }
 
 
