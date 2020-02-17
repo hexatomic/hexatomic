c742a2c25fb43326dc57ef7cdeb238420e071e59
Stephan Druskat <mail@sdruskat.net>
2020-02-17 15:50:43 +0100

    Implement naive brute-force way of calculating columns

--- bundles/org.corpus_tools.hexatomic.edit.grid/src/main/java/org/corpus_tools/hexatomic/edit/grid/data/Column.java
+++ bundles/org.corpus_tools.hexatomic.edit.grid/src/main/java/org/corpus_tools/hexatomic/edit/grid/data/Column.java
@@ -1,3 +1,24 @@
+/*-
+ * #%L
+ * org.corpus_tools.hexatomic.edit.grid
+ * %%
+ * Copyright (C) 2018 - 2020 Stephan Druskat,
+ *                                     Thomas Krause
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 package org.corpus_tools.hexatomic.edit.grid.data;
 
 import java.util.HashMap;
@@ -31,23 +52,25 @@ public class Column {
   // Column() {}
 
   /**
-   * TODO
+   * TODO.
    * 
-   * @param rowIndex
-   * @return
+   * @param rowIndex TODO
+   * @return TODO
    */
   LabelableElement getDataObject(int rowIndex) {
     return rowCells.get(rowIndex);
   }
 
-  /**
-   * TODO
-   * 
-   * @param rowIndex
-   * @param anno
-   * @return
-   * @return
-   */
+  // /**
+  // * TODO.
+  // *
+  // * @param rowIndex
+  // * @param anno
+  // * @return
+  // * @return
+  // * @return
+  // * @throws CellExistsException
+  // */
   // boolean setRow(int rowIndex, SAnnotation anno) {
   // boolean isNewValue = rowCells.get(rowIndex) == null;
   // if (isNewValue) {
@@ -56,13 +79,34 @@ public class Column {
   // return isNewValue;
   // }
 
-  public boolean setRow(int rowIndex, LabelableElement dataObject) {
+  /**
+   * TODO .
+   * 
+   * @param rowIndex TODO
+   * @return TODO
+   */
+  boolean checkRowExists(int rowIndex) {
+    if (rowCells.get(rowIndex) != null) {
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * TODO .
+   * 
+   * @param rowIndex TODO
+   * @param dataObject TODO
+   * @throws CellExistsException TODO
+   */
+  void setRow(int rowIndex, LabelableElement dataObject) throws RuntimeException {
     LabelableElement existingDataObject = rowCells.get(rowIndex);
     if (existingDataObject != null) {
-      return false;
+      throw new RuntimeException("Row " + rowIndex + " in column " + this.getTitle()
+          + " already holds a data object.\n Tried to add: " + dataObject + "\nAlready held: "
+          + existingDataObject);
     } else {
       rowCells.put(rowIndex, dataObject);
-      return true;
     }
   }
 
@@ -82,6 +126,34 @@ public class Column {
   void setTitle(String title) {
     this.title = title;
 
+  }
+
+  // /**
+  // * Package-protected constructor setting the row cells value.
+  // *
+  // * @param rowIndex The index at which the annotation should be added to the list of row cells
+  // * @param anno The annotation to add to the list of row cells
+  // */
+  // Column(int rowIndex, SAnnotation anno) {
+  // rowCells.add(rowIndex, anno);
+  // }
+  //
+  // Column() {}
+
+  public String getTitle() {
+    return title;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder(title == null ? "null" : title);
+    sb.append(":\n");
+    for (int i = 0; i < rowCells.size(); i++) {
+      String t = getDisplayText(i) == null ? "null" : getDisplayText(i);
+      Object o = getDataObject(i) == null ? "null" : getDataObject(i).getClass().getSimpleName();
+      sb.append(t + " (" + o + ").");
+    }
+    return sb.toString();
   }
 
 
