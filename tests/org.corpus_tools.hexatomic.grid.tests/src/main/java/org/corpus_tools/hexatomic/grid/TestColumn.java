b6948a4520d3b9ec0e7511236610386e659d618c
Stephan Druskat <mail@sdruskat.net>
2020-02-18 09:52:51 +0100

    Add test case for Column

---
+++ tests/org.corpus_tools.hexatomic.grid.tests/src/main/java/org/corpus_tools/hexatomic/grid/TestColumn.java
@@ -0,0 +1,76 @@
+package org.corpus_tools.hexatomic.grid;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Stephan Druskat (mail@sdruskat.net)
+ *
+ */
+class TestColumn {
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @BeforeEach
+  void setUp() throws Exception {}
+
+  /**
+   * Test method for {@link org.corpus_tools.hexatomic.grid.data.Column#getDataObject(int)}.
+   */
+  @Test
+  final void testGetDataObject() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link org.corpus_tools.hexatomic.grid.data.Column#isRowEmpty(int)}.
+   */
+  @Test
+  final void testIsRowEmpty() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link org.corpus_tools.hexatomic.grid.data.Column#areRowsEmpty(int, int)}.
+   */
+  @Test
+  final void testAreRowsEmpty() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link org.corpus_tools.hexatomic.grid.data.Column#setRow(int, org.corpus_tools.salt.graph.LabelableElement)}.
+   */
+  @Test
+  final void testSetRow() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link org.corpus_tools.hexatomic.grid.data.Column#getDisplayText(int)}.
+   */
+  @Test
+  final void testGetDisplayText() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link org.corpus_tools.hexatomic.grid.data.Column#setTitle(java.lang.String)}.
+   */
+  @Test
+  final void testSetTitle() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link org.corpus_tools.hexatomic.grid.data.Column#getHeader()}.
+   */
+  @Test
+  final void testGetHeader() {
+    fail("Not yet implemented"); // TODO
+  }
+
+}
