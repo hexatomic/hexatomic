7b501a83852199fa027a315659d6058a60efc8e3
Thomas Krause <krauseto@hu-berlin.de>
2020-02-20 16:18:46 +0100

    Ask user if they want to close the application even if there are unsaved changes.

--- bundles/org.corpus_tools.hexatomic.core/src/main/java/org/corpus_tools/hexatomic/core/handlers/ExitHandler.java
+++ bundles/org.corpus_tools.hexatomic.core/src/main/java/org/corpus_tools/hexatomic/core/handlers/ExitHandler.java
@@ -1,6 +1,9 @@
 package org.corpus_tools.hexatomic.core.handlers;
 
 import javax.inject.Inject;
+import javax.inject.Named;
+import org.corpus_tools.hexatomic.core.CommandParams;
+import org.corpus_tools.hexatomic.core.ProjectManager;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.di.UIEventTopic;
@@ -10,9 +13,13 @@ import org.eclipse.e4.ui.workbench.IWorkbench;
 import org.eclipse.e4.ui.workbench.UIEvents;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.e4.ui.workbench.modeling.IWindowCloseHandler;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
 
 public class ExitHandler implements IWindowCloseHandler {
 
+  @Inject
+  private ProjectManager projectManager;
 
   @Inject
   @Optional
@@ -24,8 +31,12 @@ public class ExitHandler implements IWindowCloseHandler {
   }
 
   @Execute
-  public void execute(IWorkbench workbench, MWindow window) {
-    if (this.close(window)) {
+  protected void execute(IWorkbench workbench, MWindow window,
+      @Optional @Named(CommandParams.FORCE_CLOSE) String forceCloseRaw) {
+
+    boolean forceClose = Boolean.parseBoolean(forceCloseRaw);
+
+    if (forceClose || this.close(window)) {
       workbench.close();
     }
   }
@@ -33,6 +44,18 @@ public class ExitHandler implements IWindowCloseHandler {
   @Override
   public boolean close(MWindow window) {
     // TODO: Check if the project has been saved
+    if (projectManager.isDirty()) {
+      // Ask user if project should be closed even with unsaved changes
+      boolean confirmed = MessageDialog.openConfirm((Shell) window.getWidget(),
+          "Unsaved changes in project",
+          "There are unsaved changes in the project that whill be lost if you close the application. "
+              + "Do you really want to close Hexatomic?");
+      if (confirmed) {
+        return true;
+      } else {
+        return false;
+      }
+    }
     return true;
   }
 }
