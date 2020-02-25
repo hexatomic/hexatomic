7a3b7a635d36ebcc8c8cff1c3a1ffc789468eed4
Stephan Druskat <mail@sdruskat.net>
2020-02-25 14:52:32 +0100

    Test example corpus

--- tests/org.corpus_tools.hexatomic.grid.tests/src/main/java/org/corpus_tools/hexatomic/grid/data/TestGraphDataProvider.java
+++ tests/org.corpus_tools.hexatomic.grid.tests/src/main/java/org/corpus_tools/hexatomic/grid/data/TestGraphDataProvider.java
@@ -1,12 +1,18 @@
 package org.corpus_tools.hexatomic.grid.data;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
+import java.io.File;
+import org.corpus_tools.salt.SaltFactory;
 import org.corpus_tools.salt.common.SDocumentGraph;
 import org.corpus_tools.salt.common.STextualDS;
+import org.corpus_tools.salt.common.SaltProject;
+import org.eclipse.emf.common.util.URI;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -19,6 +25,8 @@ import org.junit.jupiter.api.Test;
 class TestGraphDataProvider {
 
   private static GraphDataProvider fixture = null;
+  private SDocumentGraph exampleGraph;
+  private STextualDS exampleText;
 
   /**
    * @throws java.lang.Exception
@@ -26,6 +34,8 @@ class TestGraphDataProvider {
   @BeforeEach
   void setUp() throws Exception {
     fixture = new GraphDataProvider();
+    exampleGraph = buildSimpleGraph();
+    exampleText = getFirstTextFromGraph(exampleGraph);
   }
 
   /**
@@ -65,8 +75,33 @@ class TestGraphDataProvider {
    * {@link org.corpus_tools.hexatomic.grid.data.GraphDataProvider#getDataValue(int, int)}.
    */
   @Test
-  final void testGetDataValue() {
-    fail("Not yet implemented"); // TODO
+  final void testGetDataValueDefaultExample() {
+    fixture.setGraph(exampleGraph);
+    fixture.setDsAndResolveGraph(exampleText);
+
+    assertEquals("Is", fixture.getDataValue(0, 0));
+    assertEquals("this", fixture.getDataValue(0, 1));
+    assertEquals("example", fixture.getDataValue(0, 2));
+    assertEquals("more", fixture.getDataValue(0, 3));
+    assertEquals("complicated", fixture.getDataValue(0, 4));
+    assertEquals("than", fixture.getDataValue(0, 5));
+    assertEquals("it", fixture.getDataValue(0, 6));
+    assertEquals("appears", fixture.getDataValue(0, 7));
+    assertEquals("to", fixture.getDataValue(0, 8));
+    assertEquals("be", fixture.getDataValue(0, 9));
+    assertEquals("?", fixture.getDataValue(0, 10));
+
+    assertEquals("be", fixture.getDataValue(1, 0));
+    assertEquals("this", fixture.getDataValue(1, 1));
+    assertEquals("example", fixture.getDataValue(1, 2));
+    assertEquals("more", fixture.getDataValue(1, 3));
+    assertEquals("complicated", fixture.getDataValue(1, 4));
+    assertEquals("than", fixture.getDataValue(1, 5));
+    assertEquals("it", fixture.getDataValue(1, 6));
+    assertEquals("appear", fixture.getDataValue(1, 7));
+    assertEquals("to", fixture.getDataValue(1, 8));
+    assertEquals("be", fixture.getDataValue(1, 9));
+    assertEquals("?", fixture.getDataValue(1, 10));
   }
 
   /**
@@ -102,6 +137,25 @@ class TestGraphDataProvider {
   @Test
   final void testSetDsAndResolveGraph() {
     fail("Not yet implemented"); // TODO
+  }
+
+  private SDocumentGraph buildSimpleGraph() {
+    File exampleProjectDirectory =
+        new File("src/main/resources/org/corpus_tools/hexatomic/grid/example-corpus/");
+    assertTrue(exampleProjectDirectory.isDirectory());
+    URI exampleProjectUri = URI.createFileURI(exampleProjectDirectory.getAbsolutePath());
+    SaltProject project = SaltFactory.createSaltProject();
+    project.loadSaltProject(exampleProjectUri);
+    SDocumentGraph graph =
+        project.getCorpusGraphs().get(0).getDocuments().get(0).getDocumentGraph();
+    assertNotNull(graph);
+    return graph;
+  }
+
+  private STextualDS getFirstTextFromGraph(SDocumentGraph graph) {
+    STextualDS text = graph.getTextualDSs().get(0);
+    assertNotNull(text);
+    return text;
   }
 
 }
