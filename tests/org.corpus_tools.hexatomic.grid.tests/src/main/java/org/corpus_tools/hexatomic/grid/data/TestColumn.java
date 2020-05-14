7612cbe3f43aa61311fc7e4dc533c82424466ac4
Stephan Druskat <mail@sdruskat.net>
2020-05-14 13:05:41 +0200

    Fix setting values on annotations and add tests

--- tests/org.corpus_tools.hexatomic.grid.tests/src/main/java/org/corpus_tools/hexatomic/grid/data/TestColumn.java
+++ tests/org.corpus_tools.hexatomic.grid.tests/src/main/java/org/corpus_tools/hexatomic/grid/data/TestColumn.java
@@ -1,6 +1,5 @@
 package org.corpus_tools.hexatomic.grid.data;
 
-import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -25,9 +24,12 @@ import org.junit.jupiter.api.Test;
  */
 class TestColumn {
 
-  private static Column fixture = null;
-  private SToken token = null;
-  private SAnnotation annotation = null;
+  private static Column fixtureTokenTextColumn = null;
+  private static Column fixtureAnnotationColumn = null;
+  private SToken token1 = null;
+  private SToken token2 = null;
+  private SAnnotation annotation1 = null;
+  private SAnnotation annotation2 = null;
 
   /**
    * Sets up the fixture for unit tests.
@@ -38,17 +40,30 @@ class TestColumn {
   void setUp() throws Exception {
     SDocumentGraph graph = mock(SDocumentGraph.class);
 
-    token = mock(SToken.class);
-    when(token.getGraph()).thenReturn(graph);
-    when(graph.getText(token)).thenReturn("Test");
+    token1 = mock(SToken.class);
+    when(token1.getGraph()).thenReturn(graph);
+    when(graph.getText(token1)).thenReturn("Unit");
+    token2 = mock(SToken.class);
+    when(token2.getGraph()).thenReturn(graph);
+    when(graph.getText(token2)).thenReturn("Test");
 
-    annotation = mock(SAnnotation.class);
-    when(annotation.getValue()).thenReturn("testAnno");
+    fixtureTokenTextColumn = new Column(ColumnType.TOKEN_TEXT, "Header");
+    fixtureTokenTextColumn.setRow(0, token1);
+    fixtureTokenTextColumn.setRow(1, token2);
 
-    fixture = new Column(ColumnType.TOKEN_ANNOTATION);
-    fixture.setHeader("Header");
-    fixture.setRow(0, token);
-    fixture.setRow(1, annotation);
+    annotation1 = mock(SAnnotation.class);
+    when(annotation1.getQName()).thenReturn("test::anno");
+    when(annotation1.getValue()).thenReturn("anno1");
+    annotation2 = mock(SAnnotation.class);
+    when(annotation2.getQName()).thenReturn("test::anno");
+    when(annotation2.getValue()).thenReturn("anno2");
+
+    when(token1.getAnnotation(annotation1.getQName())).thenReturn(annotation1);
+    when(token2.getAnnotation(annotation2.getQName())).thenReturn(annotation2);
+
+    fixtureAnnotationColumn = new Column(ColumnType.TOKEN_ANNOTATION, annotation1.getQName());
+    fixtureAnnotationColumn.setRow(0, token1);
+    fixtureAnnotationColumn.setRow(1, token2);
   }
 
   /**
@@ -57,11 +72,12 @@ class TestColumn {
    */
   @Test
   final void testSetRow() {
-    fixture.setRow(100, token);
-    assertEquals(token, fixture.getCells().get(100));
-    assertTrue(fixture.getBits().get(100));
-    assertEquals(3, fixture.getBits().cardinality());
-    assertThrows(RuntimeException.class, () -> fixture.setRow(0, mock(SToken.class)));
+    fixtureTokenTextColumn.setRow(100, token1);
+    assertEquals(token1, fixtureTokenTextColumn.getCells().get(100));
+    assertTrue(fixtureTokenTextColumn.getBits().get(100));
+    assertEquals(3, fixtureTokenTextColumn.getBits().cardinality());
+    assertThrows(RuntimeException.class,
+        () -> fixtureTokenTextColumn.setRow(0, mock(SToken.class)));
   }
 
 
@@ -71,8 +87,8 @@ class TestColumn {
    */
   @Test
   final void testGetDataObject() {
-    assertEquals(token, fixture.getDataObject(0));
-    assertEquals(annotation, fixture.getDataObject(1));
+    assertEquals(token1, fixtureTokenTextColumn.getDataObject(0));
+    assertEquals(token2, fixtureTokenTextColumn.getDataObject(1));
   }
 
   /**
@@ -80,8 +96,8 @@ class TestColumn {
    */
   @Test
   final void testIsRowEmpty() {
-    assertTrue(fixture.isRowEmpty(2));
-    assertFalse(fixture.isRowEmpty(1));
+    assertTrue(fixtureTokenTextColumn.isRowEmpty(2));
+    assertFalse(fixtureTokenTextColumn.isRowEmpty(1));
   }
 
   /**
@@ -89,10 +105,10 @@ class TestColumn {
    */
   @Test
   final void testAreRowsEmpty() {
-    assertTrue(fixture.areRowsEmpty(2, 100));
-    assertFalse(fixture.areRowsEmpty(0, 1));
-    assertFalse(fixture.areRowsEmpty(1, 2));
-    assertFalse(fixture.areRowsEmpty(1, 100));
+    assertTrue(fixtureTokenTextColumn.areRowsEmpty(2, 100));
+    assertFalse(fixtureTokenTextColumn.areRowsEmpty(0, 1));
+    assertFalse(fixtureTokenTextColumn.areRowsEmpty(1, 2));
+    assertFalse(fixtureTokenTextColumn.areRowsEmpty(1, 100));
   }
 
   /**
@@ -100,18 +116,41 @@ class TestColumn {
    */
   @Test
   final void testGetDisplayText() {
-    assertEquals("Test", fixture.getDisplayText(0));
-    assertEquals("testAnno", fixture.getDisplayText(1));
-    fixture.setRow(2, mock(SStructure.class));
-    assertNull(fixture.getDisplayText(2));
+    assertEquals("Unit", fixtureTokenTextColumn.getDisplayText(0));
+    assertEquals("Test", fixtureTokenTextColumn.getDisplayText(1));
+    assertEquals("anno1", fixtureAnnotationColumn.getDisplayText(0));
+    assertEquals("anno2", fixtureAnnotationColumn.getDisplayText(1));
   }
 
   /**
-   * Test method for {@link org.corpus_tools.hexatomic.grid.data.Column#getDisplayText(int)}.
+   * Test method for {@link org.corpus_tools.hexatomic.grid.data.Column#getDisplayText(int)} which
+   * triggers a RuntimeException by adding the wrong type of data object to the token text type
+   * column.
+   */
+  @Test
+  final void testGetDisplayTextThrowsException() {
+    assertEquals("Unit", fixtureTokenTextColumn.getDisplayText(0));
+    assertEquals("Test", fixtureTokenTextColumn.getDisplayText(1));
+    SStructure structure = mock(SStructure.class);
+    fixtureTokenTextColumn.setRow(2, structure);
+    RuntimeException e =
+        assertThrows(RuntimeException.class, () -> fixtureTokenTextColumn.getDisplayText(2));
+    assertEquals(
+        "A column of type TOKEN_TEXT can only contain data objects of type org.corpus_tools.salt.common.SToken. Encountered: class org.corpus_tools.salt.common.SStructure$MockitoMock",
+        e.getMessage().split("\\$[\\d]+")[0]);
+  }
+
+  /**
+   * Test method for {@link org.corpus_tools.hexatomic.grid.data.Column#getHeader()}.
    */
   @Test
   final void testGetHeader() {
-    assertEquals("Header", fixture.getHeader());
+    assertEquals("Header", fixtureTokenTextColumn.getHeader());
+    assertEquals("test::anno", fixtureAnnotationColumn.getHeader());
+
+    // Test indexed header
+    Column indexedColumn = new Column(ColumnType.SPAN_ANNOTATION, "test", 2);
+    assertEquals("test (2)", indexedColumn.getHeader());
   }
 
 }
