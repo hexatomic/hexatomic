1c187dc50c7eefd55654d7734c154cad1166fb1b
Stephan Druskat <mail@sdruskat.net>
2020-05-15 23:56:19 +0200

    Fix Checkstyle violations

--- tests/org.corpus_tools.hexatomic.grid.tests/src/main/java/org/corpus_tools/hexatomic/grid/data/TestGraphDataProvider.java
+++ tests/org.corpus_tools.hexatomic.grid.tests/src/main/java/org/corpus_tools/hexatomic/grid/data/TestGraphDataProvider.java
@@ -323,15 +323,15 @@ class TestGraphDataProvider {
 
     // Remove single cell span annotation and delete span
     assertEquals("val_span_1", fixture.getDataValue(2, 0));
-    SStructuredNode nodeToRemove = fixture.getNode(2, 0);
+    final SStructuredNode originalNodeToRemove1 = fixture.getNode(2, 0);
     fixture.setDataValue(2, 0, null);
     assertNull(fixture.getDataValue(2, 0));
     assertNull(fixture.getNode(2, 0));
-    assertNull(overlappingExampleGraph.getNode(nodeToRemove.getId()));
+    assertNull(overlappingExampleGraph.getNode(originalNodeToRemove1.getId()));
 
     // Remove data from one span cell in multi-cell annotation and remove span
     assertEquals("val_span_2", fixture.getDataValue(2, 1));
-    nodeToRemove = fixture.getNode(2, 1);
+    final SStructuredNode originalNodeToRemove2 = fixture.getNode(2, 1);
     fixture.setDataValue(2, 1, null);
     assertNull(fixture.getDataValue(2, 1));
     assertNull(fixture.getNode(2, 1));
@@ -341,11 +341,11 @@ class TestGraphDataProvider {
     assertNull(fixture.getNode(2, 3));
     assertNull(fixture.getDataValue(2, 4));
     assertNull(fixture.getNode(2, 4));
-    assertNull(overlappingExampleGraph.getNode(nodeToRemove.getId()));
+    assertNull(overlappingExampleGraph.getNode(originalNodeToRemove2.getId()));
 
     // Remove token annotation but don't remove token
     assertEquals("six_tok_anno_3", fixture.getDataValue(1, 3));
-    SStructuredNode nodeNotToRemove = fixture.getNode(1, 3);
+    final SStructuredNode nodeNotToRemove = fixture.getNode(1, 3);
     fixture.setDataValue(1, 3, null);
     assertNull(fixture.getDataValue(1, 3));
     assertNull(fixture.getNode(1, 3));
