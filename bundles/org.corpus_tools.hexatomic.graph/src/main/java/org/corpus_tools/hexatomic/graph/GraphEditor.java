3d178f951ed4b37fafd567ea19a2f501bf5a3069
Thomas Krause <krauseto@hu-berlin.de>
2020-05-28 08:04:13 +0200

    Revert "Trying to figure out what wents wrong on the CI servers"
    This reverts commit 3a916d1b0e8adbd1684ef0bf6c2275e76742d11c.

--- bundles/org.corpus_tools.hexatomic.graph/src/main/java/org/corpus_tools/hexatomic/graph/GraphEditor.java
+++ bundles/org.corpus_tools.hexatomic.graph/src/main/java/org/corpus_tools/hexatomic/graph/GraphEditor.java
@@ -685,9 +685,6 @@ public class GraphEditor {
     Optional<Graph<?, ?, ?>> changedGraph = SaltHelper.getGraphForObject(element);
     if (changedGraph.isPresent() && loadedGraph.getId().equals(changedGraph.get().getId())) {
       updateView(true, false);
-    } else {
-      // TODO: this is just for testing the CI, remove it when finished testing!
-      updateView(true, false);
     }
   }
 
