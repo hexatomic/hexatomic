ebf88a0d697a761480cb08529f1ea0db8ff1fdca
Stephan Druskat <mail@sdruskat.net>
2020-05-29 14:45:47 +0200

    Register command handlers in column header layer

--- bundles/org.corpus_tools.hexatomic.grid/src/main/java/org/corpus_tools/hexatomic/grid/layers/CustomColumnHeaderLayer.java
+++ bundles/org.corpus_tools.hexatomic.grid/src/main/java/org/corpus_tools/hexatomic/grid/layers/CustomColumnHeaderLayer.java
@@ -23,12 +23,12 @@ package org.corpus_tools.hexatomic.grid.layers;
 
 import java.util.Map.Entry;
 import org.corpus_tools.hexatomic.grid.data.Column;
-import org.corpus_tools.hexatomic.grid.data.ColumnHeaderDataProvider;
 import org.corpus_tools.hexatomic.grid.data.DataUtil;
 import org.corpus_tools.hexatomic.grid.data.GraphDataProvider;
+import org.corpus_tools.hexatomic.grid.handlers.DisplayAnnotationRenameDialogCommandHandler;
+import org.corpus_tools.hexatomic.grid.handlers.RenameAnnotationCommandHandler;
 import org.corpus_tools.salt.common.SStructuredNode;
 import org.corpus_tools.salt.core.SAnnotation;
-import org.corpus_tools.salt.util.SaltUtil;
 import org.eclipse.nebula.widgets.nattable.columnRename.event.RenameColumnHeaderEvent;
 import org.eclipse.nebula.widgets.nattable.data.IDataProvider;
 import org.eclipse.nebula.widgets.nattable.grid.layer.ColumnHeaderLayer;
@@ -70,33 +70,31 @@ public class CustomColumnHeaderLayer extends ColumnHeaderLayer {
    * Renames the column in the NatTable.
    * 
    * <p>
-   * This action (1) changes the underlying {@link Column}'s value (from which the header label is
+   * This (1) changes the underlying {@link Column}'s value (from which the header label is
    * computed), and (2) changes the qualified names of all annotations in the underlying
    * {@link Column} cells to the namespace::name combination computed from the
    * <code>customColumnName</code> parameter.
    * </p>
    */
   @Override
-  public boolean renameColumnPosition(int columnPosition, String customColumnName) {
-    IDataProvider dataProvider =
+  public boolean renameColumnPosition(int columnPosition, String newQName) {
+    log.debug("New qualified name: {}", newQName);
+    IDataProvider columnHeaderDataProvider =
         ((DefaultColumnHeaderDataLayer) this.getBaseLayer()).getDataProvider();
     int idx = getColumnIndexByPosition(columnPosition);
-    Object oldHeader = ((ColumnHeaderDataProvider) dataProvider).getDataValue(idx, 0);
+
     // Get the respective Column object from the underlying data model.
     // Note that this is the NatTable's column index as the empty corner "column" is discounted.
     Column column = graphDataProvider.getColumns().get(idx);
     String oldQName = column.getColumnValue();
-    boolean renamed = (!oldHeader.equals(customColumnName) && (!oldQName.equals(customColumnName)));
+
+    boolean renamed = (!oldQName.equals(newQName));
     if (renamed) {
-      String namespace = DataUtil.splitNamespaceFromQNameString(customColumnName);
-      if (namespace == null) {
-        namespace = SaltUtil.SALT_NAMESPACE;
-      }
-      String name = DataUtil.splitNameFromQNameString(customColumnName);
-      // Sets the column value via the ColumnHeaderDataProvider
-      dataProvider.setDataValue(idx, 0, namespace + SaltUtil.NAMESPACE_SEPERATOR + name);
-      log.debug("Set column value at index {} (old value: '{}' to '{}'.", idx, oldHeader,
-          customColumnName);
+      // Set the new qName in the column header
+      columnHeaderDataProvider.setDataValue(idx, 0, newQName);
+      log.debug("Set column value at index {} (old value: '{}') to '{}'.", idx, oldQName, newQName);
+
+      // Rename annotations in the column
       for (Entry<Integer, SStructuredNode> cellEntry : column.getRowCells().entrySet()) {
         SStructuredNode node = cellEntry.getValue();
         SAnnotation annotation = node.getAnnotation(oldQName);
@@ -105,18 +103,25 @@ public class CustomColumnHeaderLayer extends ColumnHeaderLayer {
           log.debug("Renaming annotation '{}' on node '{}'.", annotation.toString(),
               node.getName());
           node.removeLabel(oldQName);
-          node.createAnnotation(namespace, name, value);
+          SAnnotation newAnnotation =
+              node.createAnnotation(DataUtil.splitNamespaceFromQNameString(newQName),
+                  DataUtil.splitNameFromQNameString(newQName), value);
           // Should ideally work like this:
           // annotation.setNamespace(namespace);
           // annotation.setName(name);
-          log.debug("Column {}: Renamed annotation on node {} to '{}::{}={}'.",
-              column.getColumnValue(), node.getName(), annotation.getNamespace(),
-              annotation.getName(), annotation.getValue());
+          log.debug("Renamed annotation on node {} from {} to '{}'.", node.getName(),
+              annotation.getQName(), newAnnotation.getQName());
         }
         fireLayerEvent(new RenameColumnHeaderEvent(this, columnPosition));
       }
     }
     return renamed;
+  }
+
+  @Override
+  protected void registerCommandHandlers() {
+    registerCommandHandler(new RenameAnnotationCommandHandler(this));
+    registerCommandHandler(new DisplayAnnotationRenameDialogCommandHandler(this));
   }
 
 }
