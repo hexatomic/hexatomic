bc40e073cf1e70613c3132538583afc014977908
Stephan Druskat <mail@sdruskat.net>
2020-06-04 17:00:19 +0200

    Explicitly set keyboard layout to EN_US for integration tests
    - This circumvents detecting the local layout, but may let fail tests testing keypresses for
    OS keyboards that differ from EN_US.

--- tests/org.corpus_tools.hexatomic.it.tests/src/main/java/org/corpus_tools/hexatomic/it/tests/TestCorpusStructure.java
+++ tests/org.corpus_tools.hexatomic.it.tests/src/main/java/org/corpus_tools/hexatomic/it/tests/TestCorpusStructure.java
@@ -41,6 +41,7 @@ class TestCorpusStructure {
   void setup() {
     org.eclipse.swtbot.swt.finder.utils.SWTBotPreferences.KEYBOARD_STRATEGY =
         "org.eclipse.swtbot.swt.finder.keyboard.SWTKeyboardStrategy";
+    org.eclipse.swtbot.swt.finder.utils.SWTBotPreferences.KEYBOARD_LAYOUT = "EN_US";
 
     IEclipseContext ctx = ContextHelper.getEclipseContext();
 
