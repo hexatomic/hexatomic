dc7dacd2e1d5f7866d5056edc66f336f505b18a2
Thomas Krause <krauseto@hu-berlin.de>
2020-08-18 12:27:15 +0200

    Add more complete test that checks if undo restores all changes made by removing all nodes and relations

--- tests/org.corpus_tools.hexatomic.core.tests/src/main/java/org/corpus_tools/hexatomic/core/TestProjectManager.java
+++ tests/org.corpus_tools.hexatomic.core.tests/src/main/java/org/corpus_tools/hexatomic/core/TestProjectManager.java
@@ -18,6 +18,8 @@ import static org.mockito.Mockito.when;
 import java.io.File;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -26,6 +28,7 @@ import org.corpus_tools.hexatomic.core.events.salt.SaltNotificationFactory;
 import org.corpus_tools.salt.common.SDocument;
 import org.corpus_tools.salt.common.SDocumentGraph;
 import org.corpus_tools.salt.common.SToken;
+import org.corpus_tools.salt.core.SNode;
 import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
@@ -34,6 +37,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestProjectManager {
+
+  private static final String DOC1_ID = "salt:/rootCorpus/subCorpus1/doc1";
+  private static final String DOC3_ID = "salt:/rootCorpus/subCorpus2/doc3";
 
   private static final String TEST_ANNO_QNAME = "test::anno";
 
@@ -59,12 +65,11 @@ class TestProjectManager {
     exampleProjectUri = URI.createFileURI(exampleProjectDirectory.getAbsolutePath());
 
     events = mock(IEventBroker.class);
-    when(events.send(anyString(), any()))
-        .then(invocation -> {
-          projectManager.subscribeUndoOperationAdded(invocation.getArgument(1));
-          return true;
-        });
-    
+    when(events.send(anyString(), any())).then(invocation -> {
+      projectManager.subscribeUndoOperationAdded(invocation.getArgument(1));
+      return true;
+    });
+
     errorService = mock(ErrorService.class);
     partService = mock(EPartService.class);
     uiStatus = mock(UiStatusReport.class);
@@ -109,23 +114,21 @@ class TestProjectManager {
   public void testDocumentCleanup() {
     projectManager.open(exampleProjectUri);
 
-    String documentID = "salt:/rootCorpus/subCorpus2/doc3";
-
     // Load document once
-    Optional<SDocument> document = projectManager.getDocument(documentID, true);
+    Optional<SDocument> document = projectManager.getDocument(DOC3_ID, true);
     assertTrue(document.isPresent());
     assertNotNull(document.get().getDocumentGraphLocation());
     assertNotNull(document.get().getDocumentGraph());
 
     // Mock three open documents: two of them are the same
     Map<String, String> state1 = new HashMap<>();
-    state1.put(DOCUMENT_ID, documentID);
+    state1.put(DOCUMENT_ID, DOC3_ID);
 
     Map<String, String> state2 = new HashMap<>();
     state2.put(DOCUMENT_ID, "salt:/rootCorpus/subCorpus2/doc4");
 
     Map<String, String> state3 = new HashMap<>();
-    state3.put(DOCUMENT_ID, documentID);
+    state3.put(DOCUMENT_ID, DOC3_ID);
 
     MPart editor1 = mock(MPart.class);
     MPart editor2 = mock(MPart.class);
@@ -138,14 +141,14 @@ class TestProjectManager {
     when(partService.getParts()).thenReturn(Arrays.asList(editor1, editor2, editor3));
 
     // Notify one of the documents was closed
-    projectManager.unloadDocumentGraphWhenClosed(documentID);
+    projectManager.unloadDocumentGraphWhenClosed(DOC3_ID);
     // Document must still be loaded in memory
     assertNotNull(document.get().getDocumentGraph());
 
     // Remove the one of the closed parts
     when(partService.getParts()).thenReturn(Arrays.asList(editor2, editor3));
     // Notify about closing one of the documents again
-    projectManager.unloadDocumentGraphWhenClosed(documentID);
+    projectManager.unloadDocumentGraphWhenClosed(DOC3_ID);
     // Document should have been unloaded from memory
     assertNull(document.get().getDocumentGraph());
   }
@@ -159,8 +162,7 @@ class TestProjectManager {
 
 
     // Get the document graph and change the label of the first label a few times
-    String documentID = "salt:/rootCorpus/subCorpus2/doc3";
-    Optional<SDocument> document = projectManager.getDocument(documentID, true);
+    Optional<SDocument> document = projectManager.getDocument(DOC3_ID, true);
     assertTrue(document.isPresent());
     if (document.isPresent()) {
       SDocumentGraph docGraph = document.get().getDocumentGraph();
@@ -227,6 +229,45 @@ class TestProjectManager {
       assertEquals("3", token.get(0).getAnnotation(TEST_ANNO_QNAME).getValue());
       assertTrue(projectManager.canUndo());
       assertFalse(projectManager.canRedo());
+    }
+  }
+
+  /**
+   * Deletes all elements of a document graph and checks if the changes can be undone.
+   */
+  @Test
+  public void testUndoDeletion() {
+    projectManager.open(exampleProjectUri);
+
+    // Documents 1 and 3 should be the same
+    Optional<SDocument> document1 = projectManager.getDocument(DOC1_ID, true);
+    Optional<SDocument> document3 = projectManager.getDocument(DOC3_ID, true);
+    assertTrue(document1.isPresent());
+    assertTrue(document3.isPresent());
+
+    if (document1.isPresent() && document3.isPresent()) {
+      SDocumentGraph docGraph1 = document1.get().getDocumentGraph();
+      assertNotNull(docGraph1);
+
+      SDocumentGraph docGraph3 = document3.get().getDocumentGraph();
+      assertNotNull(docGraph3);
+
+      assertEquals(new HashSet<>(), docGraph1.findDiffs(docGraph3));
+
+      docGraph1.removeRelations();
+      List<SNode> nodes = new LinkedList<>(docGraph1.getNodes());
+      for (SNode n : nodes) {
+        docGraph1.removeNode(n);
+      }
+      // This has changed the document graph
+      assertFalse(docGraph1.findDiffs(docGraph3).isEmpty());
+
+      projectManager.addCheckpoint();
+
+      projectManager.undo();
+
+      // Test that the graphs are equal again
+      assertEquals(new HashSet<>(), docGraph1.findDiffs(docGraph3));
     }
   }
 }
