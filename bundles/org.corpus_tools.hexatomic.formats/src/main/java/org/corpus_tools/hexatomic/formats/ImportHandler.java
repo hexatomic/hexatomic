ad7f6c263551f465bb07fceaa3020263e3eb01ee
Thomas Krause <krauseto@hu-berlin.de>
2020-08-21 14:56:09 +0200

    Instantiate PepperImpl directly

--- bundles/org.corpus_tools.hexatomic.formats/src/main/java/org/corpus_tools/hexatomic/formats/ImportHandler.java
+++ bundles/org.corpus_tools.hexatomic.formats/src/main/java/org/corpus_tools/hexatomic/formats/ImportHandler.java
@@ -1,14 +1,20 @@
 package org.corpus_tools.hexatomic.formats;
 
+import org.corpus_tools.hexatomic.core.errors.ErrorService;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.widgets.Shell;
 
 public class ImportHandler {
 
-	@Execute
-	protected void execute(Shell shell) {
-		WizardDialog dialog = new WizardDialog(shell, new ImportWizard());
-		dialog.open();
-	}
+  @Execute
+  protected static void execute(Shell shell, ErrorService errorService) {
+    try {
+      WizardDialog dialog = new WizardDialog(shell, new ImportWizard(errorService));
+      dialog.open();
+    } catch (Exception e) {
+      errorService.handleException("Could not initialize Pepper modules", e,
+          PepperImportWorkflow.class);
+    }
+  }
 }
--- bundles/org.corpus_tools.hexatomic.formats/src/main/java/org/corpus_tools/hexatomic/formats/ImportWizard.java
+++ bundles/org.corpus_tools.hexatomic.formats/src/main/java/org/corpus_tools/hexatomic/formats/ImportWizard.java
@@ -2,7 +2,11 @@ package org.corpus_tools.hexatomic.formats;
 
 import java.io.File;
 import java.util.Optional;
+import org.corpus_tools.hexatomic.core.errors.ErrorService;
 import org.corpus_tools.hexatomic.formats.CorpusPathSelectionPage.Type;
+import org.corpus_tools.pepper.common.Pepper;
+import org.corpus_tools.pepper.common.PepperConfiguration;
+import org.corpus_tools.pepper.core.PepperImpl;
 import org.eclipse.jface.wizard.Wizard;
 
 public class ImportWizard extends Wizard {
@@ -10,9 +14,14 @@ public class ImportWizard extends Wizard {
   private final CorpusPathSelectionPage corpusPathPage = new CorpusPathSelectionPage(Type.Import);
   private final ImporterSelectionPage importerPage = new ImporterSelectionPage();
 
-  public ImportWizard() {
+  public ImportWizard(ErrorService errorService) {
     super();
     setNeedsProgressMonitor(true);
+
+    PepperConfiguration pepperConf = new PepperConfiguration();
+    Pepper pepper = new PepperImpl();
+    pepper.setConfiguration(pepperConf);
+
   }
 
   @Override
@@ -24,6 +33,7 @@ public class ImportWizard extends Wizard {
   public void addPages() {
     addPage(corpusPathPage);
     addPage(importerPage);
+
   }
 
   @Override
@@ -31,7 +41,9 @@ public class ImportWizard extends Wizard {
     Optional<File> corpusPath = corpusPathPage.getCorpusPath();
     Optional<Format> selectedFormat = importerPage.getSelectedFormat();
     if (corpusPath.isPresent() && selectedFormat.isPresent()) {
-
+      PepperImportWorkflow workflow = new PepperImportWorkflow();
+      workflow.convert();
+      return true;
     }
     return false;
   }
