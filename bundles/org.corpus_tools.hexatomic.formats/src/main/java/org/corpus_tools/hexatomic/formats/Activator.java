166adece8863a9d034a3d385697e90256de3d2ed
Thomas Krause <krauseto@hu-berlin.de>
2020-08-21 15:59:57 +0200

    Also set the resource folder property

--- bundles/org.corpus_tools.hexatomic.formats/src/main/java/org/corpus_tools/hexatomic/formats/Activator.java
+++ bundles/org.corpus_tools.hexatomic.formats/src/main/java/org/corpus_tools/hexatomic/formats/Activator.java
@@ -2,6 +2,7 @@ package org.corpus_tools.hexatomic.formats;
 
 import java.util.Optional;
 import org.corpus_tools.pepper.common.Pepper;
+import org.corpus_tools.pepper.common.PepperConfiguration;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
@@ -35,6 +36,17 @@ public class Activator implements BundleActivator {
       }
     }
 
+    // Add and start all required pepper module bundles
+    // TODO: bundle the JAR files with Hexatomic
+    System.getProperties().setProperty(PepperConfiguration.PROP_PEPPER_MODULE_RESOURCES,
+        "/home/thomas/src/pepper/pepper-lib/target/distribution/pepper-lib-3.4.2-SNAPSHOT/org.corpus-tools.pepper-lib_3.4.2-SNAPSHOT/plugins/");
+    String[] bundleLocationsToLoad = {
+        "file:/home/thomas/src/pepper/pepper-lib/target/distribution/pepper-lib-3.4.2-SNAPSHOT/org.corpus-tools.pepper-lib_3.4.2-SNAPSHOT/plugins/exmaralda-emf-api-1.2.1.jar   ",
+        "file:/home/thomas/src/pepper/pepper-lib/target/distribution/pepper-lib-3.4.2-SNAPSHOT/org.corpus-tools.pepper-lib_3.4.2-SNAPSHOT/plugins/pepperModules-EXMARaLDAModules-1.3.0.jar"};
+    for (String bundleLocation : bundleLocationsToLoad) {
+      Bundle b = context.installBundle(bundleLocation);
+      b.start();
+    }
   }
 
   @Override
