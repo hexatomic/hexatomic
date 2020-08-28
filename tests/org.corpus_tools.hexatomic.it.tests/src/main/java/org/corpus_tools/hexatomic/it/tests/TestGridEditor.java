c7348642a265d3b1c313d8d51825f787f65dae52
Stephan Druskat <sdruskat@users.noreply.github.com>
2020-08-28 13:19:00 +0200

    Update tests/org.corpus_tools.hexatomic.it.tests/src/main/java/org/corpus_tools/hexatomic/it/tests/TestGridEditor.java
    Co-authored-by: Thomas Krause <thomaskrause@users.noreply.github.com>

--- tests/org.corpus_tools.hexatomic.it.tests/src/main/java/org/corpus_tools/hexatomic/it/tests/TestGridEditor.java
+++ tests/org.corpus_tools.hexatomic.it.tests/src/main/java/org/corpus_tools/hexatomic/it/tests/TestGridEditor.java
@@ -561,7 +561,7 @@ public class TestGridEditor {
     SWTBotRootMenu contextMenu = table.contextMenu(1, 1);
     // No context menu should exist
     assertTrue(contextMenu.menuItems().isEmpty());
-    assertThrows(WidgetNotFoundException.class, () -> contextMenu.contextMenu());
+    assertThrows(WidgetNotFoundException.class, contextMenu::contextMenu);
   }
 
 }
