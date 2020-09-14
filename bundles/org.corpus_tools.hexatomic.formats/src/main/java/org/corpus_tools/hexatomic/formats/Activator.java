9cbce8ac2293c11fc59b217da89471154a506c19
Thomas Krause <krauseto@hu-berlin.de>
2020-09-14 16:27:36 +0200

    Bundle the necessary Exmaralda JAR bundle files

--- bundles/org.corpus_tools.hexatomic.formats/src/main/java/org/corpus_tools/hexatomic/formats/Activator.java
+++ bundles/org.corpus_tools.hexatomic.formats/src/main/java/org/corpus_tools/hexatomic/formats/Activator.java
@@ -1,8 +1,11 @@
 package org.corpus_tools.hexatomic.formats;
 
+import java.io.File;
+import java.net.URL;
 import java.util.Optional;
 import org.corpus_tools.pepper.common.Pepper;
 import org.corpus_tools.pepper.common.PepperConfiguration;
+import org.eclipse.core.runtime.FileLocator;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
@@ -36,15 +39,17 @@ public class Activator implements BundleActivator {
       }
     }
 
-    // Add and start all required pepper module bundles
-    // TODO: bundle the JAR files with Hexatomic
+    // Add and start all required pepper module bundles in the correct order
+    File pluginsFolder =
+        new File(FileLocator.resolve(context.getBundle().getEntry("pepper-plugins")).toURI());
     System.getProperties().setProperty(PepperConfiguration.PROP_PEPPER_MODULE_RESOURCES,
-        "/home/thomas/src/pepper/pepper-lib/target/distribution/pepper-lib-3.4.2-SNAPSHOT/org.corpus-tools.pepper-lib_3.4.2-SNAPSHOT/plugins/");
+        pluginsFolder.getAbsolutePath());
     String[] bundleLocationsToLoad = {
-        "file:/home/thomas/src/pepper/pepper-lib/target/distribution/pepper-lib-3.4.2-SNAPSHOT/org.corpus-tools.pepper-lib_3.4.2-SNAPSHOT/plugins/exmaralda-emf-api-1.2.1.jar   ",
-        "file:/home/thomas/src/pepper/pepper-lib/target/distribution/pepper-lib-3.4.2-SNAPSHOT/org.corpus-tools.pepper-lib_3.4.2-SNAPSHOT/plugins/pepperModules-EXMARaLDAModules-1.3.0.jar"};
-    for (String bundleLocation : bundleLocationsToLoad) {
-      Bundle b = context.installBundle(bundleLocation);
+        "exmaralda-emf-api-1.2.1.jar", "pepperModules-EXMARaLDAModules-1.3.0.jar"};
+    for (String location : bundleLocationsToLoad) {
+      URL fileUrl = context.getBundle().getEntry("pepper-plugins/" + location);
+      File f = new File(FileLocator.resolve(fileUrl).toURI());
+      Bundle b = context.installBundle("file:" + f.getAbsolutePath());
       b.start();
     }
   }
