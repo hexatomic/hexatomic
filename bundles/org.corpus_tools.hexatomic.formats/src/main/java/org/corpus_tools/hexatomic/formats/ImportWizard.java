f27b6503c7d5a0250d54c0bf78e6c2cca41ef6b5
Thomas Krause <krauseto@hu-berlin.de>
2020-09-14 15:48:04 +0200

    Show progress when importing documents

--- bundles/org.corpus_tools.hexatomic.formats/src/main/java/org/corpus_tools/hexatomic/formats/ImportWizard.java
+++ bundles/org.corpus_tools.hexatomic.formats/src/main/java/org/corpus_tools/hexatomic/formats/ImportWizard.java
@@ -2,14 +2,21 @@ package org.corpus_tools.hexatomic.formats;
 
 import java.io.File;
 import java.lang.reflect.InvocationTargetException;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 import org.corpus_tools.hexatomic.core.ProjectManager;
 import org.corpus_tools.hexatomic.core.errors.ErrorService;
 import org.corpus_tools.hexatomic.formats.CorpusPathSelectionPage.Type;
 import org.corpus_tools.pepper.common.CorpusDesc;
+import org.corpus_tools.pepper.common.DOCUMENT_STATUS;
+import org.corpus_tools.pepper.common.JOB_STATUS;
 import org.corpus_tools.pepper.common.Pepper;
 import org.corpus_tools.pepper.common.PepperJob;
 import org.corpus_tools.pepper.common.StepDesc;
+import org.corpus_tools.pepper.core.PepperJobImpl;
+import org.corpus_tools.pepper.modules.DocumentController;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.jface.wizard.Wizard;
 
@@ -57,16 +64,61 @@ public class ImportWizard extends Wizard {
       importStep.setCorpusDesc(corpusDesc);
       // TODO: add properties for the importer
       job.addStepDesc(importStep);
+      
+
 
       try {
-
         // Execute the conversion as task that can be aborted
         getContainer().run(true, true, (monitor) -> {
-          job.convertFrom();
 
+          monitor.beginTask("Importing corpus structure", IProgressMonitor.UNKNOWN);
+
+          // Create a set of already finished documents
+          Set<String> completedDocuments = new HashSet<>();
+
+          // Run conversion in a background thread so we can add regular status reports
+          Thread background = new Thread(job::convertFrom);
+          background.start();
+
+          Optional<Integer> numberOfJobs = Optional.empty();
+
+
+          while (background.isAlive()) {
+            if (monitor.isCanceled()) {
+              background.interrupt();
+            }
+            // Check if any new document has been finished
+            if (job instanceof PepperJobImpl) {
+              PepperJobImpl pepperJobImpl = (PepperJobImpl) job;
+              
+              JOB_STATUS jobStatus = pepperJobImpl.getStatus();
+              if (!numberOfJobs.isPresent()
+                  && jobStatus == JOB_STATUS.IMPORTING_DOCUMENT_STRUCTURE) {
+                // We don't know how many documents are present yet but since importing the
+                // documents has started, we can get this number
+                numberOfJobs = Optional.of(pepperJobImpl.getDocumentControllers().size());
+                monitor.beginTask("Importing " + numberOfJobs.get() + " documents",
+                    numberOfJobs.get());
+              }
+
+              if (numberOfJobs.isPresent()) {
+                for (DocumentController controller : pepperJobImpl.getDocumentControllers()) {
+                  DOCUMENT_STATUS docStatus = controller.getGlobalStatus();
+                  if (docStatus == DOCUMENT_STATUS.COMPLETED || docStatus == DOCUMENT_STATUS.DELETED
+                      || docStatus == DOCUMENT_STATUS.FAILED) {
+                    if (completedDocuments.add(controller.getGlobalId())) {
+                      monitor.worked(1);
+                    }
+                  }
+                }
+              }
+            }
+            Thread.sleep(500);
+          }
           // set the corpus as current project
           projectManager.newProject();
           projectManager.setProject(job.getSaltProject());
+          monitor.done();
 
         });
 
