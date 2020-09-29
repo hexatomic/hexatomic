c294621da6c42798301b2d304e921f70be02b8e0
Stephan Druskat <mail@sdruskat.net>
2020-09-29 22:26:17 +0200

    Replace anonymous inner class with lambda

--- bundles/org.corpus_tools.hexatomic.grid/src/main/java/org/corpus_tools/hexatomic/grid/internal/configuration/ColumnHeaderUiBindingConfiguration.java
+++ bundles/org.corpus_tools.hexatomic.grid/src/main/java/org/corpus_tools/hexatomic/grid/internal/configuration/ColumnHeaderUiBindingConfiguration.java
@@ -21,14 +21,11 @@
 package org.corpus_tools.hexatomic.grid.internal.configuration;
 
 import org.corpus_tools.hexatomic.grid.internal.GridHelper;
-import org.eclipse.nebula.widgets.nattable.NatTable;
 import org.eclipse.nebula.widgets.nattable.config.AbstractUiBindingConfiguration;
 import org.eclipse.nebula.widgets.nattable.coordinate.PositionCoordinate;
 import org.eclipse.nebula.widgets.nattable.selection.SelectionLayer;
-import org.eclipse.nebula.widgets.nattable.ui.action.IMouseAction;
 import org.eclipse.nebula.widgets.nattable.ui.binding.UiBindingRegistry;
 import org.eclipse.nebula.widgets.nattable.ui.matcher.MouseEventMatcher;
-import org.eclipse.swt.events.MouseEvent;
 
 /**
  * A custom UI binding configuration for the column header.
@@ -48,18 +45,14 @@ public class ColumnHeaderUiBindingConfiguration extends AbstractUiBindingConfigu
 
     // Bind mouse double-clicks to renaming the annotation name
     uiBindingRegistry.registerDoubleClickBinding(MouseEventMatcher.columnHeaderLeftClick(0),
-        new IMouseAction() {
-          @Override
-          public void run(NatTable natTable, MouseEvent event) {
-            PositionCoordinate[] selectedCellCoordinates =
-                selectionLayer.getSelectedCellPositions();
-            // Get column and check if it's the token column
-            for (PositionCoordinate coord : selectedCellCoordinates) {
-              if (GridHelper.isTokenColumnAtPosition(natTable, coord.getColumnPosition(), false)) {
-                System.err.println("TOKEN, IGNORING");
-              } else {
-                System.out.println("Double Click Detected on " + coord.getColumnPosition());
-              }
+        (natTable, event) -> {
+          PositionCoordinate[] selectedCellCoordinates = selectionLayer.getSelectedCellPositions();
+          // Get column and check if it's the token column
+          for (PositionCoordinate coord : selectedCellCoordinates) {
+            if (GridHelper.isTokenColumnAtPosition(natTable, coord.getColumnPosition(), false)) {
+              System.err.println("TOKEN, IGNORING");
+            } else {
+              System.out.println("Double Click Detected on " + coord.getColumnPosition());
             }
           }
         });
