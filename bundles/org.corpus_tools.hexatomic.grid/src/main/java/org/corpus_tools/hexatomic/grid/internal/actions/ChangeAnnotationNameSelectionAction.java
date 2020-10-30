5b39482110acb1cf78dd917456b2d7beda425507
Stephan Druskat <mail@sdruskat.net>
2020-10-30 10:12:17 +0100

    Add action to trigger rename command on cells

--- bundles/org.corpus_tools.hexatomic.grid/src/main/java/org/corpus_tools/hexatomic/grid/internal/actions/ChangeAnnotationNameSelectionAction.java
+++ bundles/org.corpus_tools.hexatomic.grid/src/main/java/org/corpus_tools/hexatomic/grid/internal/actions/ChangeAnnotationNameSelectionAction.java
@@ -4,25 +4,24 @@
 package org.corpus_tools.hexatomic.grid.internal.actions;
 
 import java.util.Set;
-import org.corpus_tools.hexatomic.grid.internal.commands.ChangeAnnotationNameSelectionCommand;
+import org.corpus_tools.hexatomic.grid.internal.commands.DisplayAnnotationRenameDialogOnCellsCommand;
 import org.eclipse.nebula.widgets.nattable.NatTable;
 import org.eclipse.nebula.widgets.nattable.coordinate.PositionCoordinate;
 import org.eclipse.nebula.widgets.nattable.ui.action.IKeyAction;
 import org.eclipse.swt.events.KeyEvent;
 
 /**
- * An {@link IKeyAction} that triggers a {@link ChangeAnnotationNameSelectionCommand} on all
- * selected non-token cells of this instance.
+ * An {@link IKeyAction} that triggers a {@link DisplayAnnotationRenameDialogOnCellsCommand} for all
+ * selected non-token cells of this instance at once.
  * 
  * @author Stephan Druskat {@literal <mail@sdruskat.net>}
- *
  */
 public class ChangeAnnotationNameSelectionAction implements IKeyAction {
 
   private final Set<PositionCoordinate> selectedNonTokenCells;
 
   /**
-   * Create a new action with a set of {@link PositionCoordinate}s.
+   * Creates a new action with a set of {@link PositionCoordinate}s.
    * 
    * @param selectedNonTokenCells The set of position coordinates that this action should process.
    */
@@ -32,10 +31,7 @@ public class ChangeAnnotationNameSelectionAction implements IKeyAction {
 
   @Override
   public void run(NatTable natTable, KeyEvent event) {
-    for (PositionCoordinate cell : this.selectedNonTokenCells) {
-      natTable.doCommand(new ChangeAnnotationNameSelectionCommand(null, cell.getColumnPosition(),
-          cell.getRowPosition()));
-    }
+    natTable.doCommand(new DisplayAnnotationRenameDialogOnCellsCommand(this.selectedNonTokenCells));
   }
 
 }
