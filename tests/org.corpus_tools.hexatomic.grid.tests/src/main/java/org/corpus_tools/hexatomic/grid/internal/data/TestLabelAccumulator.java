cb2b876703785911d66b62f1c2deb197469b42c6
Stephan Druskat <mail@sdruskat.net>
2020-11-12 15:12:34 +0100

    Let label accumulator tests use example data

--- tests/org.corpus_tools.hexatomic.grid.tests/src/main/java/org/corpus_tools/hexatomic/grid/internal/data/TestLabelAccumulator.java
+++ tests/org.corpus_tools.hexatomic.grid.tests/src/main/java/org/corpus_tools/hexatomic/grid/internal/data/TestLabelAccumulator.java
@@ -4,18 +4,11 @@ import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-import java.util.List;
-import org.corpus_tools.hexatomic.grid.internal.data.Column.ColumnType;
+import org.corpus_tools.hexatomic.grid.internal.TestHelper;
 import org.corpus_tools.hexatomic.grid.style.StyleConfiguration;
-import org.corpus_tools.salt.common.SSpan;
-import org.corpus_tools.salt.common.SSpanningRelation;
-import org.corpus_tools.salt.common.SToken;
 import org.eclipse.nebula.widgets.nattable.layer.LabelStack;
 import org.eclipse.nebula.widgets.nattable.layer.SpanningDataLayer;
-import org.eclipse.nebula.widgets.nattable.layer.cell.ILayerCell;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -28,7 +21,7 @@ import org.junit.jupiter.api.Test;
 class TestLabelAccumulator {
 
   private LabelAccumulator fixture = null;
-  private GraphDataProvider fixtureProvider = null;
+  private GraphDataProvider dataProvider = null;
   private SpanningDataLayer fixtureLayer = null;
 
   /**
@@ -36,35 +29,10 @@ class TestLabelAccumulator {
    */
   @BeforeEach
   void setUp() {
-    fixtureProvider = mock(GraphDataProvider.class);
-    fixtureLayer = mock(SpanningDataLayer.class);
-    SToken token = mock(SToken.class);
-    SSpan span = mock(SSpan.class);
-    SSpanningRelation spanningRelation = mock(SSpanningRelation.class);
-    when(spanningRelation.getSource()).thenReturn(span);
-    when(spanningRelation.getTarget()).thenReturn(token);
-    when(fixtureProvider.getDataValue(0, 0)).thenReturn(token);
-    @SuppressWarnings("unchecked")
-    List<Column> columns = mock(List.class);
-    Column tokenColumn = mock(Column.class);
-    Column spanColumn = mock(Column.class);
-    when(tokenColumn.getColumnType()).thenReturn(ColumnType.TOKEN_TEXT);
-    when(columns.get(0)).thenReturn(tokenColumn);
-    when(spanColumn.getColumnType()).thenReturn(ColumnType.SPAN_ANNOTATION);
-    when(columns.get(1)).thenReturn(spanColumn);
-    when(fixtureProvider.getColumns()).thenReturn(columns);
-    when(fixtureLayer.getColumnIndexByPosition(0)).thenReturn(0);
-    when(fixtureLayer.getColumnIndexByPosition(1)).thenReturn(1);
-    fixture = new LabelAccumulator(fixtureLayer, fixtureProvider);
-    ILayerCell tokenCell = mock(ILayerCell.class);
-    ILayerCell emptyCell = mock(ILayerCell.class);
-    ILayerCell spanCell = mock(ILayerCell.class);
-    when(tokenCell.getDataValue()).thenReturn(token);
-    when(emptyCell.getDataValue()).thenReturn(null);
-    when(spanCell.getDataValue()).thenReturn(span);
-    when(fixtureLayer.getCellByPosition(0, 0)).thenReturn(tokenCell);
-    when(fixtureLayer.getCellByPosition(1, 0)).thenReturn(spanCell);
-    when(fixtureLayer.getCellByPosition(0, 1)).thenReturn(emptyCell);
+    dataProvider = TestHelper.createOverlappingDataProvider();
+    NodeSpanningDataProvider fixtureProvider = new NodeSpanningDataProvider(dataProvider);
+    fixtureLayer = new SpanningDataLayer(fixtureProvider);
+    fixture = new LabelAccumulator(fixtureLayer, dataProvider);
   }
 
   /**
@@ -73,10 +41,10 @@ class TestLabelAccumulator {
    */
   @Test
   void testLabelAccumulator() {
-    assertThrows(IllegalArgumentException.class, () -> new LabelAccumulator(null, fixtureProvider));
+    assertThrows(IllegalArgumentException.class, () -> new LabelAccumulator(null, dataProvider));
     assertThrows(IllegalArgumentException.class, () -> new LabelAccumulator(fixtureLayer, null));
     assertThrows(IllegalArgumentException.class, () -> new LabelAccumulator(null, null));
-    assertDoesNotThrow(() -> new LabelAccumulator(fixtureLayer, fixtureProvider));
+    assertDoesNotThrow(() -> new LabelAccumulator(fixtureLayer, dataProvider));
   }
 
   /**
@@ -95,15 +63,15 @@ class TestLabelAccumulator {
     final LabelStack emptyStack = new LabelStack("");
     final LabelStack tokenStack = new LabelStack("");
     final LabelStack spanStack = new LabelStack("");
-    fixture.accumulateConfigLabels(emptyStack, 0, 1);
+    fixture.accumulateConfigLabels(emptyStack, 1, 1);
     assertTrue(emptyStack.getLabels().contains(StyleConfiguration.EMPTY_CELL_STYLE));
-    // Also has token text cell style and empty string
-    assertEquals(3, emptyStack.getLabels().size());
+    // // Also has empty string
+    assertEquals(2, emptyStack.getLabels().size());
     fixture.accumulateConfigLabels(tokenStack, 0, 0);
     assertTrue(tokenStack.getLabels().contains(StyleConfiguration.TOKEN_TEXT_CELL_STYLE));
     // Also has empty string
     assertEquals(2, tokenStack.getLabels().size());
-    fixture.accumulateConfigLabels(spanStack, 1, 0);
+    fixture.accumulateConfigLabels(spanStack, 3, 0);
     assertTrue(spanStack.getLabels().contains(StyleConfiguration.SPAN_ANNOTATION_CELL_STYLE));
     assertEquals(2, spanStack.getLabels().size());
   }
