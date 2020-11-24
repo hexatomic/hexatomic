1bb4ac6ede87f483630988130090b0c2452195eb
Thomas Krause <thomaskrause@posteo.de>
2020-11-24 18:24:20 +0100

    Remove unused and untested code

--- bundles/org.corpus_tools.hexatomic.formats/src/main/java/org/corpus_tools/hexatomic/formats/ConfigurationPage.java
+++ bundles/org.corpus_tools.hexatomic.formats/src/main/java/org/corpus_tools/hexatomic/formats/ConfigurationPage.java
@@ -21,17 +21,12 @@
 package org.corpus_tools.hexatomic.formats;
 
 import java.util.Properties;
-import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.wizard.WizardPage;
 
 public abstract class ConfigurationPage extends WizardPage {
 
   protected ConfigurationPage(String pageName) {
     super(pageName);
-  }
-
-  protected ConfigurationPage(String pageName, String title, ImageDescriptor titleImage) {
-    super(pageName, title, titleImage);
   }
 
   public abstract Properties getConfiguration();
