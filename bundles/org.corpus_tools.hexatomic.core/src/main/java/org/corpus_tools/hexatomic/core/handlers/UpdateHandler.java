2cd09c72af0d1c888053dbada83894fbd5620811
clachenmaier <clara.lachenmaier@uni-jena.de>
2021-03-03 14:25:42 +0100

    Update Application model

---
+++ bundles/org.corpus_tools.hexatomic.core/src/main/java/org/corpus_tools/hexatomic/core/handlers/UpdateHandler.java
@@ -0,0 +1,12 @@
+ 
+package org.corpus_tools.hexatomic.core.handlers;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+
+public class UpdateHandler {
+	@Execute
+	public void execute() {
+		
+	}
+		
+}
