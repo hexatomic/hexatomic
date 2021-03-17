202f804b40ca07ca693dd5e02659699bab082c6e
Thomas Krause <thomaskrause@posteo.de>
2021-03-17 18:24:24 +0100

    Adapt DummySync to new interface

--- tests/org.corpus_tools.hexatomic.core.tests/src/main/java/org/corpus_tools/hexatomic/core/DummySync.java
+++ tests/org.corpus_tools.hexatomic.core.tests/src/main/java/org/corpus_tools/hexatomic/core/DummySync.java
@@ -12,4 +12,19 @@ public final class DummySync extends UISynchronize {
   public void asyncExec(Runnable runnable) {
     runnable.run();
   }
+
+  @Override
+  protected boolean isUIThread(Thread thread) {
+    return false;
+  }
+
+  @Override
+  protected void showBusyWhile(Runnable runnable) {
+    // Not implemented in dummy
+  }
+
+  @Override
+  protected boolean dispatchEvents() {
+    return false;
+  }
 }
