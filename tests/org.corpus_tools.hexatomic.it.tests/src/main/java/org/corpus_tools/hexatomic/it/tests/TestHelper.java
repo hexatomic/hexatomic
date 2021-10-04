d88c92542424bf8dc68006617a166f03b3df47a9
Stephan Druskat <hexatomic@sdruskat.net>
2021-10-04 15:11:36 +0200

    Add helper method to delete directory recursively

--- tests/org.corpus_tools.hexatomic.it.tests/src/main/java/org/corpus_tools/hexatomic/it/tests/TestHelper.java
+++ tests/org.corpus_tools.hexatomic.it.tests/src/main/java/org/corpus_tools/hexatomic/it/tests/TestHelper.java
@@ -1,5 +1,11 @@
 package org.corpus_tools.hexatomic.it.tests;
 
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.HashMap;
 import java.util.Map;
 import org.corpus_tools.hexatomic.core.CommandParams;
@@ -56,10 +62,8 @@ public class TestHelper {
             forcedKeyboardLayout;
       }
     } catch (SecurityException ex) {
-      log.error(
-          "Could not get environment variable " + SWTBOT_KEYBOARD_LAYOUT
-              + " because the security mananger is running and disallowed access",
-          ex);
+      log.error("Could not get environment variable " + SWTBOT_KEYBOARD_LAYOUT
+          + " because the security mananger is running and disallowed access", ex);
     }
   }
 
@@ -77,6 +81,30 @@ public class TestHelper {
     ParameterizedCommand cmd = commandService
         .createCommand("org.corpus_tools.hexatomic.core.command.new_salt_project", params);
     handlerService.executeHandler(cmd);
+  }
+
+  /**
+   * Recursively delete a directory.
+   * 
+   * @param directory The directory {@link Path} to delete recursively
+   * @return If the directory exists
+   * @throws IOException An error that occurred when reading or deleting from the file system
+   */
+  public static boolean deleteDirectory(Path directory) throws IOException {
+    Path startPath = Files.walkFileTree(directory, new SimpleFileVisitor<Path>() {
+      @Override
+      public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        Files.delete(file);
+        return FileVisitResult.CONTINUE;
+      }
+
+      @Override
+      public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        Files.delete(dir);
+        return FileVisitResult.CONTINUE;
+      }
+    });
+    return Files.notExists(startPath);
   }
 
 }
