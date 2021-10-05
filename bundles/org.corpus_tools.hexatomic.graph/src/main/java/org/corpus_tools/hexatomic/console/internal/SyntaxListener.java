a00556cbba46bc4d3d09579e7f7e09da1fe7ae2a
Stephan Druskat <hexatomic@sdruskat.net>
2021-10-05 12:23:54 +0200

    Remove unneeded imports

--- bundles/org.corpus_tools.hexatomic.graph/src/main/java/org/corpus_tools/hexatomic/console/internal/SyntaxListener.java
+++ bundles/org.corpus_tools.hexatomic.graph/src/main/java/org/corpus_tools/hexatomic/console/internal/SyntaxListener.java
@@ -21,8 +21,6 @@
 
 package org.corpus_tools.hexatomic.console.internal;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
