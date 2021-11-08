1e05ce84718ac63005cb3ca278736ef884ad0d15
clachenmaier <clara.lachenmaier@uni-jena.de>
2021-11-08 15:58:04 +0100

    Add startup update in Activator class

--- bundles/org.corpus_tools.hexatomic.updates/src/main/java/org/corpus_tools/hexatomic/updates/Activator.java
+++ bundles/org.corpus_tools.hexatomic.updates/src/main/java/org/corpus_tools/hexatomic/updates/Activator.java
@@ -20,11 +20,20 @@
 
 package org.corpus_tools.hexatomic.updates;
 
+import java.lang.reflect.InvocationTargetException;
 import javax.annotation.PostConstruct;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.preferences.ConfigurationScope;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.equinox.p2.core.IProvisioningAgent;
+import org.eclipse.equinox.p2.operations.UpdateOperation;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
 
 public class Activator implements BundleActivator {
   private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(Activator.class);
@@ -36,14 +45,72 @@ public class Activator implements BundleActivator {
   public void start(BundleContext context) throws Exception {
     log.info("Starting Updates-Plugin");
     // TODO Auto-generated method stub
-    boolean test = prefs.getBoolean("autoUpdate", false);
-    System.out.println(test);
+    if (prefs.getBoolean("autoUpdate", false)) {
+      ServiceReference<?> reference = context
+          .getServiceReference(IProvisioningAgent.SERVICE_NAME);
+      Object service = context.getService(reference);
+      if (service != null) {
+        context.ungetService(reference);
+      }
+      IProvisioningAgent agent = (IProvisioningAgent) service;
+      checkForUpdates(agent);
+      MessageDialog.openInformation(
+          null, 
+          "Updates installed", 
+          "Updates have been installed successfully." 
+          + "Restart Hexatomic to implement the changes.");
+    }
   }
 
   @Override
   public void stop(BundleContext context) throws Exception {
     // TODO Auto-generated method stub
     
+  }
+  
+  /**
+   * sdfsfda.
+   * @param agent bsadlfs
+   */
+  public static void checkForUpdates(IProvisioningAgent agent) { 
+    try { 
+      ProgressMonitorDialog progressDialog = new ProgressMonitorDialog(null); 
+      progressDialog.run(true, true, (IRunnableWithProgress) new IRunnableWithProgress() {     
+        public void run(IProgressMonitor monitor) 
+            throws InvocationTargetException, InterruptedException { 
+          doCheckForUpdates(monitor, agent);
+        } 
+      }); 
+    } catch (InvocationTargetException e) { 
+      log.info("Fehlera " + e); 
+    } catch (InterruptedException e) { 
+      log.info("Fehlerb " + e); 
+    } 
+   
+  }
+  
+  /**
+   * llafds.
+   * @param monitor dsafdsf
+   * @param agent asfdsf
+   */
+  
+  private static void doCheckForUpdates(IProgressMonitor monitor, IProvisioningAgent agent) { 
+    if (agent == null) {
+      log.info("No provisioning agent found.  This application is not set up for updates."); 
+      return; 
+    } 
+
+    try { 
+      IStatus updateStatus = P2Util.performUpdates(agent, monitor); 
+      log.info("Status" + updateStatus); 
+      if (updateStatus.getCode() == UpdateOperation.STATUS_NOTHING_TO_UPDATE) { 
+        return; 
+      } 
+
+    } finally { 
+      System.out.println("All done");
+    } 
   }
   
 
--- bundles/org.corpus_tools.hexatomic.updates/src/main/java/org/corpus_tools/hexatomic/updates/P2Util.java
+++ bundles/org.corpus_tools.hexatomic.updates/src/main/java/org/corpus_tools/hexatomic/updates/P2Util.java
@@ -26,6 +26,7 @@ import org.corpus_tools.hexatomic.core.ProjectManager;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.e4.ui.di.UISynchronize;
 import org.eclipse.e4.ui.workbench.IWorkbench;
 import org.eclipse.equinox.p2.core.IProvisioningAgent;
@@ -61,48 +62,9 @@ public class P2Util {
       IWorkbench workbench,
       UISynchronize sync,
       IProgressMonitor monitor) {
-    //Überprüfen, ob alle argumente valide sind
-    if (agent != null) {
-      log.info("Provisioning agent created");
-    } else {
-      log.info("Agent not valid");
-    }
-    if (workbench != null) {
-      log.info("Workbench created");
-    } else {
-      log.info("Workbench not valid");
-    }
-    if (monitor != null) {
-      log.info("Monitor created");
-    } else {
-      log.info("Monitor not valid");
-    }
-    if (sync != null) {
-      log.info("sync created");
-    } else {
-      log.info("sync not valid");
-    }
-    System.out.println(checkComponents(agent, workbench, sync, monitor));
+    UpdateOperation operation = createUpdateOperation(agent);
     
-    
-    ProvisioningSession session = new ProvisioningSession(agent);
-    if (session != null) {
-      log.info("Provisioning session created");
-    }
-    // update all user-visible installable units
-    UpdateOperation operation = new UpdateOperation(session);
-    URI uri = null;
-
-    try {
-      uri = new URI(REPOSITORY_LOC);
-    } catch (URISyntaxException e) {
-      //errorService.handleException("Invalid repository location " + path.toString(), e);
-      throw new OperationCanceledException("Invalid repository location");
-
-    }
-    
-    operation.getProvisioningContext().setArtifactRepositories(uri);
-    operation.getProvisioningContext().setMetadataRepositories(uri);
+    //Check if there are Updates available
     IStatus status = operation.resolveModal(monitor);
     if (status.getCode() == UpdateOperation.STATUS_NOTHING_TO_UPDATE) {
       MessageDialog.openInformation(
@@ -111,8 +73,10 @@ public class P2Util {
               "Nothing to update");
     }
     
+    //create update job
     ProvisioningJob provisioningJob = operation.getProvisioningJob(null);
     
+    //run update job
     if (provisioningJob != null) {
       sync.syncExec(new Runnable() {
  
@@ -124,6 +88,8 @@ public class P2Util {
                   "There are updates available. Do you want to install them now?");
           if (performUpdate) {
             provisioningJob.schedule();
+            
+            //restart Application if wanted
             boolean restart = MessageDialog.openQuestion(null,
                 "Updates installed, restart?",
                 "Updates have been installed successfully, do you want to restart?");
@@ -150,20 +116,80 @@ public class P2Util {
     
   }
   
+  
   /**
-  * shfiaosf.
-  * @param agent bla
-  * @param workbench bla
-  * @param sync bla
-  * @param monitor bla
-  * @return nlala
+  * ojfsdasof.
+  * @param agent blabla
+  * @param monitor blabal
+  * @return blalna
+  * @throws OperationCanceledException blabla
   */
-  public boolean checkComponents(IProvisioningAgent agent, 
+     
+  static IStatus performUpdates(IProvisioningAgent agent, IProgressMonitor monitor) 
+      throws OperationCanceledException { 
+    
+    UpdateOperation operation = createUpdateOperation(agent);
+    SubMonitor sub = SubMonitor.convert(monitor, "Checking for application updates...", 200); 
+    IStatus status = operation.resolveModal(sub.newChild(100)); 
+    if (status.getCode() == UpdateOperation.STATUS_NOTHING_TO_UPDATE) { 
+      return status; 
+    } 
+    if (status.getSeverity() == IStatus.CANCEL) {
+      throw new OperationCanceledException(); 
+    }
+       
+    if (status.getSeverity() != IStatus.ERROR) { 
+      // More complex status handling might include showing the user what 
+      // updates are available if there are multiples, differentiating 
+      // patches vs. updates, etc. In this example, we simply update as 
+      // suggested by the operation. 
+      ProvisioningJob job = operation.getProvisioningJob(monitor); 
+      if (job == null) { 
+        log.info("ProvisioningJob could not be created - " 
+            + "does this application support p2 software installation?");
+        return status;
+      } 
+      status = job.runModal(sub.newChild(100)); 
+      if (status.getSeverity() == IStatus.CANCEL) { 
+        throw new OperationCanceledException(); 
+      }
+    }  
+    return status; 
+  }
+  
+
+  static boolean checkComponents(IProvisioningAgent agent, 
       IWorkbench workbench,
       UISynchronize sync,
       IProgressMonitor monitor) {
     return ((agent != null && workbench != null && sync != null && monitor != null));
-  }  
+  }
+  
+  static UpdateOperation createUpdateOperation(IProvisioningAgent agent) {
+    ProvisioningSession session = new ProvisioningSession(agent);
+    if (session != null) {
+      log.info("Provisioning session created");
+    }
+    // update all user-visible installable units
+    UpdateOperation operation = new UpdateOperation(session);
+    URI uri = null;
+
+    try {
+      uri = new URI(REPOSITORY_LOC);
+    } catch (URISyntaxException e) {
+      throw new OperationCanceledException("Invalid repository location");
+
+    }
+    
+    //Define location of p2-repository
+    operation.getProvisioningContext().setArtifactRepositories(uri);
+    operation.getProvisioningContext().setMetadataRepositories(uri);
+    return operation;
+    
+  }
+  
+  
+  
 }
 
 
