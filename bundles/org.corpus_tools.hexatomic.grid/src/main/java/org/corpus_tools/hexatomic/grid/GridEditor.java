f06e3da97125615fec17f29698871e1c17bb9d47
Stephan Druskat <mail@sdruskat.net>
2021-11-18 11:46:08 +0100

    Add additional check for selection type before refreshing

--- bundles/org.corpus_tools.hexatomic.grid/src/main/java/org/corpus_tools/hexatomic/grid/GridEditor.java
+++ bundles/org.corpus_tools.hexatomic.grid/src/main/java/org/corpus_tools/hexatomic/grid/GridEditor.java
@@ -231,7 +231,9 @@ public class GridEditor {
    */
   @Inject
   void setSelection(@Optional @Named(IServiceConstants.ACTIVE_SELECTION) STextualDS ds) {
-    if (ds != null && ds != this.activeDs) {
+    if (selectionService.getSelection() instanceof STextualDS &&
+        ds != null &&
+        ds != this.activeDs) {
       log.debug("The textual data source {} has been selected.", ds.getId());
       this.activeDs = ds;
       bodyDataProvider.resolveDataSource(ds);
