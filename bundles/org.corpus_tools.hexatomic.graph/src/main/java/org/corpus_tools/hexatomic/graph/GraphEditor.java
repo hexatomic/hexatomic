b4c423c6218f07b9416f36a9887d23445a2df0a7
Thomas Krause <thomas.krause@hu-berlin.de>
2021-12-13 16:46:34 +0100

    Use ChangeSet#containsDocument() to determine if the change is relevant for the graph editor.
    This is more in sync to what is suggested in the developer documentation. To make this work
    for deleted elements, we have to fix the containsDocument() function as well.

--- bundles/org.corpus_tools.hexatomic.graph/src/main/java/org/corpus_tools/hexatomic/graph/GraphEditor.java
+++ bundles/org.corpus_tools.hexatomic.graph/src/main/java/org/corpus_tools/hexatomic/graph/GraphEditor.java
@@ -35,7 +35,6 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.TreeSet;
-import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
@@ -45,7 +44,6 @@ import org.corpus_tools.hexatomic.core.Topics;
 import org.corpus_tools.hexatomic.core.errors.ErrorService;
 import org.corpus_tools.hexatomic.core.handlers.OpenSaltDocumentHandler;
 import org.corpus_tools.hexatomic.core.undo.ChangeSet;
-import org.corpus_tools.hexatomic.core.undo.ReversibleOperation;
 import org.corpus_tools.hexatomic.graph.internal.GraphDragMoveAdapter;
 import org.corpus_tools.hexatomic.graph.internal.RootTraverser;
 import org.corpus_tools.hexatomic.graph.internal.SaltGraphContentProvider;
@@ -54,7 +52,6 @@ import org.corpus_tools.hexatomic.graph.internal.SaltGraphStyler;
 import org.corpus_tools.salt.SALT_TYPE;
 import org.corpus_tools.salt.common.SDocument;
 import org.corpus_tools.salt.common.SDocumentGraph;
-import org.corpus_tools.salt.common.SDocumentGraphObject;
 import org.corpus_tools.salt.common.SDominanceRelation;
 import org.corpus_tools.salt.common.SPointingRelation;
 import org.corpus_tools.salt.common.SSpan;
@@ -722,23 +719,12 @@ public class GraphEditor {
       ChangeSet changeSet = (ChangeSet) element;
       log.debug("Received ANNOTATION_CHANGED event for changeset {}", changeSet);
 
-      // Filter for change set events that belong to the loaded document graph
-      SDocumentGraph loadedGraph = getGraph();
-      Set<Object> changedObjects = changeSet.getChanges().stream().filter(c -> {
-        Object container = c.getChangedContainer();
-        if (container instanceof SDocumentGraphObject) {
-          // The container can be a node or edge but we need the actual
-          // document graph to compare it to our loaded graph.
-          SDocumentGraphObject object = (SDocumentGraphObject) container;
-          container = object.getGraph();
-        }
-        return container == loadedGraph;
-      }).map(ReversibleOperation::getChangedElement).collect(Collectors.toSet());
-
-      if (!changedObjects.isEmpty()) {
+      // check graph updates contain changes for this graph or if elements have been deleted
+      if (changeSet.containsDocument(
+          thisPart.getPersistedState().get(OpenSaltDocumentHandler.DOCUMENT_ID))) {
         // Only relations with text coverage semantics can change the structure of the graph and
         // modify segments
-        boolean recalculateSegments = changedObjects.stream().anyMatch(
+        boolean recalculateSegments = changeSet.getChangedElements().stream().anyMatch(
             c -> c instanceof STextualRelation || c instanceof STextOverlappingRelation<?, ?>);
         updateView(recalculateSegments, false);
       }
