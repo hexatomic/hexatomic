ad0e3b5a778089a6a38e83b360769253e88248c7
Thomas Krause <thomas.krause@hu-berlin.de>
2021-12-20 16:36:24 +0100

    Fix checkstyle violation

--- bundles/org.corpus_tools.hexatomic.graph/src/main/java/org/corpus_tools/hexatomic/graph/internal/AnnotationNameProposalProvider.java
+++ bundles/org.corpus_tools.hexatomic.graph/src/main/java/org/corpus_tools/hexatomic/graph/internal/AnnotationNameProposalProvider.java
@@ -1,3 +1,23 @@
+/*-
+ * #%L
+ * org.corpus_tools.hexatomic.graph
+ * %%
+ * Copyright (C) 2018 - 2021 Stephan Druskat, Thomas Krause
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 package org.corpus_tools.hexatomic.graph.internal;
 
 import java.util.Set;
@@ -24,14 +44,15 @@ class AnnotationNameProposalProvider implements IContentProposalProvider {
 
   private void calculateProposals() {
     this.proposals.clear();
-    
-    this.proposals.addAll(graph.getNodes().parallelStream().flatMap(n -> n.getAnnotations().stream())
-        .map(a -> a.getQName()).collect(Collectors.toList()));
+
+    this.proposals
+        .addAll(graph.getNodes().parallelStream().flatMap(n -> n.getAnnotations().stream())
+            .map(a -> a.getQName()).collect(Collectors.toList()));
   }
 
   @Override
   public IContentProposal[] getProposals(String contents, int position) {
-    if(position == 0) {
+    if (position == 0) {
       return null;
     }
     String string2Find = contents.substring(0, position);
