5c0503d5cfd5b299fed1c49fca79ed05b9d090a9
Stephan Druskat <mail@sdruskat.net>
2022-06-15 09:51:27 +0200

    Remove duplicate method with typo

--- bundles/org.corpus_tools.hexatomic.grid/src/main/java/org/corpus_tools/hexatomic/grid/internal/layers/GridFreezeLayer.java
+++ bundles/org.corpus_tools.hexatomic.grid/src/main/java/org/corpus_tools/hexatomic/grid/internal/layers/GridFreezeLayer.java
@@ -103,16 +103,6 @@ public class GridFreezeLayer extends CompositeFreezeLayer {
   /**
    * Triggers the merging of spans into a single span in the underlying data model.
    * 
-   * @param spans The {@link SSpan}s to merge
-   * @param coordinates The coordinates for the {@link SSpan}s to merge
-   */
-  public void mergeAnnotationSpan(List<SSpan> spans, PositionCoordinate[] coordinates) {
-    bodyDataProvider.mergeAnnotationSpans(spans, coordinates);
-  }
-
-  /**
-   * Triggers the merging of spans into a single span in the underlying data model.
-   * 
    * @param span The {@link SSpan}s to merge
    * @param coordinates The coordinates for the {@link SSpan}s to merge
    */
