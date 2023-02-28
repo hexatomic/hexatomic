9de36688f60b1ba72e1a37f65d2d03786ac6de1b
Thomas Krause <thomas.krause@hu-berlin.de>
2023-02-28 12:53:01 +0100

    Format source code

--- tests/org.corpus_tools.hexatomic.it.tests/src/main/java/org/corpus_tools/hexatomic/it/tests/TableCellEditorActiveCondition.java
+++ tests/org.corpus_tools.hexatomic.it.tests/src/main/java/org/corpus_tools/hexatomic/it/tests/TableCellEditorActiveCondition.java
@@ -4,19 +4,19 @@ import org.eclipse.swtbot.nebula.nattable.finder.widgets.SWTBotNatTable;
 import org.eclipse.swtbot.swt.finder.waits.DefaultCondition;
 
 public final class TableCellEditorActiveCondition extends DefaultCondition {
-	private final SWTBotNatTable table;
+  private final SWTBotNatTable table;
 
-	public TableCellEditorActiveCondition(SWTBotNatTable table) {
-		this.table = table;
-	}
+  public TableCellEditorActiveCondition(SWTBotNatTable table) {
+    this.table = table;
+  }
 
-	@Override
-	public boolean test() throws Exception {
-		return table.widget.getActiveCellEditor() == null;
-	}
+  @Override
+  public boolean test() throws Exception {
+    return table.widget.getActiveCellEditor() == null;
+  }
 
-	@Override
-	public String getFailureMessage() {
-		return "Setting new value for cell took too long.";
-	}
+  @Override
+  public String getFailureMessage() {
+    return "Setting new value for cell took too long.";
+  }
 }
